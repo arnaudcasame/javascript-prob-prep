d412817370d08d9fb7d4aa785ba1554343e37eb1
tizekod <noon.tizekod@gmail.com>
2023-11-12 01:43:16 -0500

    Sets up common members of set test

--- javascript/common-set-members/common-set-members.spec.js
+++ javascript/common-set-members/common-set-members.spec.js
@@ -1,9 +1,20 @@
 import { commonSetMembers } from "./common-set-members";
 
+export const common = 
 describe('Common set Members', function(){
-    test('Should return the common members of both sets', function(){
-        const A = [13, 27, 35, 40, 49, 55, 59];
-        const B = [17, 35, 39, 40, 55, 58, 60];
-        expect(commonSetMembers(A, B)).toEqual([])
+    let A;
+    let B;
+    beforeAll(function(){
+        A = [13, 27, 35, 40, 49, 55, 59, 60];
+        B = [17, 35, 39, 40, 55, 58, 60, 62];
+    })
+    test('Should return the elements in common of both sets (A<-->B)', function(){
+        const result = commonSetMembers(A, B);
+        expect(result).toEqual([35, 40, 55, 60]);
+    })
+
+    test('Should return the elements in common of both sets  (B<-->A)', function(){
+        const result = commonSetMembers(B, A);
+        expect(result).toEqual([35, 40, 55, 60]);
     })
 })
