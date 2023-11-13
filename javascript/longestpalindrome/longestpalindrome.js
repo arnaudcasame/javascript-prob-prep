f2a3351aa68b134760b659311759901882ed7300
tizekod <noon.tizekod@gmail.com>
2023-11-12 23:23:45 -0500

    Implements Palindrome Brute Force solution

--- javascript/longestpalindrome/longestpalindrome.js
+++ javascript/longestpalindrome/longestpalindrome.js
@@ -3,18 +3,22 @@
  * @return {string}
  */
 export const longestPalindrome = function(s) {
-    let i = 0;
-    let num = 0;
-    let palindrome = null;
-    for (let j = 2; j < s.length; j++) {
-        const word = s.slice(i, j);
+    let palindrome = '';
+    for (let j = 1; j < s.length; j++) {
+        const word = s.slice(0, j+1);
         const reverse = word.split('').reverse().join('');
-        console.log(word, reverse)
-        if(word === reverse && num < word.length){
-            num = word.length;
-            palindrome = word;
-        } else {
-            i++;
+        for (let i = 0; i < word.length; i++) {
+            if(word.length <= 2 || i === 0){
+                // console.log('result: ' + j + ') ' + s + ' ->', word + ' ->', reverse, i)
+                if(word === reverse && palindrome.length < word.length){
+                    palindrome = word;
+                }
+            }else{
+                // console.log('result: ' + j + ') ' + s + ' ->', word.slice(i) + ' ->', reverse.slice(0, reverse.length - i), i)
+                if((word.slice(i) === reverse.slice(0, reverse.length - i)) && (palindrome.length < word.slice(i).length)){
+                    palindrome = word.slice(i);
+                }
+            }
         }
     }
     return palindrome;
