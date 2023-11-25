5745ae6d7ccda62a61148cca8106f15f84f3f65b
Tizekod <noon.tizekod@gmail.com>
2023-11-24 23:29:06 -0500

    Calls CheckPermutation test file with Jest

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { longestSubstring } from './javascript/longestsubstring/longestsubstring.spec';
-import { longest } from "./javascript/longestpalindrome/longestpalindrome.spec";
 import { common } from "./javascript/common-set-members/common-set-members.spec";
 import { is_unique } from "./javascript/is-unique/is-unique.spec";
+import { CheckPermutation } from "./javascript/check-permutation/check-permutation.spec";
+import { longest } from "./javascript/longestpalindrome/longestpalindrome.spec";
