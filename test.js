4383fe7aa79707c383345e96dc6bb5094c9cefa6
Tizekod <noon.tizekod@gmail.com>
2023-11-25 00:51:49 -0500

    Calls tests from cci (Craking the Coding Interview) folder

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { longestSubstring } from './javascript/longestsubstring/longestsubstring.spec';
 import { common } from "./javascript/common-set-members/common-set-members.spec";
-import { is_unique } from "./javascript/is-unique/is-unique.spec";
-import { CheckPermutation } from "./javascript/check-permutation/check-permutation.spec";
+import { is_unique } from "./cci/is-unique/is-unique.spec";
+import { CheckPermutation } from "./cci/check-permutation/check-permutation.spec";
 import { longest } from "./javascript/longestpalindrome/longestpalindrome.spec";
