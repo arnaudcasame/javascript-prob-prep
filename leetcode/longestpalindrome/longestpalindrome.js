1dec7bfc41afc2cf95444ef962edfc5f7316e2f7
Tizekod <noon.tizekod@gmail.com>
2023-11-25 15:52:52 -0500

    Moves longest palindromic subsctring to leetcode folder

--- leetcode/longestpalindrome/longestpalindrome.js
+++ leetcode/longestpalindrome/longestpalindrome.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {string}
  */
-export const longestPalindrome = function(s) {
+export const longestPalindromeBF = function(s) {
     let palindrome = '';
     for (let j = 1; j < s.length; j++) {
         const word = s.slice(0, j+1);
@@ -20,6 +20,26 @@ export const longestPalindrome = function(s) {
                 }
             }
         }
+    }
+    return palindrome;
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+export const longestPalindrome = function(s) {
+    let palindrome = '';
+    let s1 = '';
+    let s2 = '';
+    const sr = s.split('').reverse().join('');
+    for (let j = 0; j < s.length; j++) {
+        const s3 = s.slice(0, sr.length - j);
+        const s4 = sr.slice(j); //[( - 1) - j]
+        const s5 = s.slice(j, s.length - j);
+        s1 += s[j];
+        s2 += sr[j];
+        console.log(j + ')', s1, s2, s3, s4, s5);
     }
     return palindrome;
 };
--- leetcode/longestpalindrome/longestpalindrome.spec.js
+++ leetcode/longestpalindrome/longestpalindrome.spec.js
@@ -1,24 +1,29 @@
-import { longestPalindrome } from './longestpalindrome';
+import { longestPalindromeBF, longestPalindrome } from './longestpalindrome';
 
 export const longest =
     describe('LongestPalindrome', () => {
         test('Longest Palindrome (babad) Should be Equal to bab or aba', () => {
+            expect(longestPalindromeBF('babad') === 'bab' || longestPalindromeBF('babad') === 'aba').toBeTruthy();
             expect(longestPalindrome('babad') === 'bab' || longestPalindrome('babad') === 'aba').toBeTruthy();
         });
 
         test('Longest Palindrome (cbbd) Should be Equal to bb', () => {
+            expect(longestPalindromeBF('cbbd')).toEqual('bb');
             expect(longestPalindrome('cbbd')).toEqual('bb');
         });
 
         test('Longest Palindrome (bbbbb) Should be Equal to bbbbb', () => {
+            expect(longestPalindromeBF('bbbbb')).toEqual('bbbbb');
             expect(longestPalindrome('bbbbb')).toEqual('bbbbb');
         });
 
         test('Longest Palindrome (pwwkew) Should be Equal to ww', () => {
+            expect(longestPalindromeBF('pwwkew')).toEqual('ww');
             expect(longestPalindrome('pwwkew')).toEqual('ww');
         });
 
         test('Longest Palindrome (eabcb) Should be Equal to bcb', () => {
+            expect(longestPalindromeBF('eabcb')).toEqual('bcb');
             expect(longestPalindrome('eabcb')).toEqual('bcb');
         });
 
