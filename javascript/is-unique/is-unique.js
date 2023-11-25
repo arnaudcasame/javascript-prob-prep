d090a41caf307bc7db0a029b9544ef2b2b181f72
Tizekod <noon.tizekod@gmail.com>
2023-11-24 20:49:27 -0500

    Implements isUnique & isUnique hash table method version

--- javascript/is-unique/is-unique.js
+++ javascript/is-unique/is-unique.js
@@ -3,6 +3,45 @@
  * Is Unique: Implement an algorithm to determine if a string has all unique characters. 
  * What if you cannot use additional data structures?
  */
-export const isUnique = () => {
-    throw new Error('Remove this statement and implement this function');
+
+/**
+ * Returns true is string only contains unique chars
+ * @param {string} s the string to evaluate
+ * @return {boolean} true of false
+ */
+export const isUnique = (s) => {
+    let i = 0, j = i+1;
+    while(i < s.length - 1){
+        if(s[i] === s[j]) {
+            console.log(s[i], s[j]);
+            return false;
+        }
+        if(j === s.length - 1){
+            i++;
+            j = i+1;
+        }else{
+            j++;
+        }
+    }
+    return true;
+};
+
+
+/** Hash Table version
+ * Returns true is string only contains unique chars
+ * @param {string} s the string to evaluate
+ * @return {boolean} true of false
+ */
+export const isUniqueHT = (s) => {
+    let i = 0;
+    const bucket = {}
+    while(i < s.length){
+        if(bucket[s[i]]){
+            return false;
+        }else{
+            bucket[s[i]] = 1;
+        }
+        i++;
+    }
+    return true;
 };
