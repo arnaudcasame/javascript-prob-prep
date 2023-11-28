563602aea6831c34429efb8cab4348d2becfd480
Tizekod <noon.tizekod@gmail.com>
2023-11-28 01:25:48 -0500

    Adds palindrome number test to main leetcode module

--- leetcode.test.js
+++ leetcode.test.js
@@ -1,3 +1,3 @@
-
 import { longest } from "./leetcode/longestpalindrome/longestpalindrome.spec";
 import { longestSubstring } from './leetcode/longestsubstring/longestsubstring.spec';
+import { numberIsPalindrome_ } from "./leetcode/palindromenumber/palindrome-number.spec";
