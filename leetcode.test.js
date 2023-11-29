113a655b2851791b40fd3ed621b78fc9352f1200
Tizekod <noon.tizekod@gmail.com>
2023-11-29 01:23:36 -0500

    Adds valid prentheses tests to main module

--- leetcode.test.js
+++ leetcode.test.js
@@ -3,3 +3,4 @@ import { longestSubstring } from './leetcode/longestsubstring/longestsubstring.s
 import { numberIsPalindrome_ } from "./leetcode/palindromenumber/palindrome-number.spec";
 import { romanToInt_ } from "./leetcode/roman-integer-13/roman-number.spec";
 import { longestCommonPrefix_ } from "./leetcode/longest-common-prefix-14/longest-common-prefix.spec";
+import { isValid_ } from "./leetcode/valid-parentheses-20/valid-parentheses.spec";
