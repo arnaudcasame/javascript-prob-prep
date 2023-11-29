7f702e476d23c92a42f9a83377dfad5fd1351e12
Tizekod <noon.tizekod@gmail.com>
2023-11-29 01:25:48 -0500

    Disables no-unused-vars ESLint rules

--- leetcode.test.js
+++ leetcode.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { longest } from "./leetcode/longestpalindrome/longestpalindrome.spec";
 import { longestSubstring } from './leetcode/longestsubstring/longestsubstring.spec';
 import { numberIsPalindrome_ } from "./leetcode/palindromenumber/palindrome-number.spec";
