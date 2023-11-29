cd547e400caa9599c4d055032ca0f0623773d6f2
Tizekod <noon.tizekod@gmail.com>
2023-11-28 22:20:59 -0500

    Adds longest common prefix test to the main module

--- leetcode.test.js
+++ leetcode.test.js
@@ -2,3 +2,4 @@ import { longest } from "./leetcode/longestpalindrome/longestpalindrome.spec";
 import { longestSubstring } from './leetcode/longestsubstring/longestsubstring.spec';
 import { numberIsPalindrome_ } from "./leetcode/palindromenumber/palindrome-number.spec";
 import { romanToInt_ } from "./leetcode/roman-integer-13/roman-number.spec";
+import { longestCommonPrefix_ } from "./leetcode/longest-common-prefix-14/longest-common-prefix.spec";
