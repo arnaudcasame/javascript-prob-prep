890e709670aff3f112fcdc29d014fd6f0088d160
Tizekod <noon.tizekod@gmail.com>
2023-11-29 23:28:17 -0500

    Adds merge-two-sorted-lists to the main test module

--- leetcode.test.js
+++ leetcode.test.js
@@ -5,3 +5,4 @@ import { numberIsPalindrome_ } from "./leetcode/palindromenumber/palindrome-numb
 import { romanToInt_ } from "./leetcode/roman-integer-13/roman-number.spec";
 import { longestCommonPrefix_ } from "./leetcode/longest-common-prefix-14/longest-common-prefix.spec";
 import { isValid_ } from "./leetcode/valid-parentheses-20/valid-parentheses.spec";
+import { mergeTwoLists_ } from "./leetcode/merge-two-lists-21/merge-two-lists.spec";
