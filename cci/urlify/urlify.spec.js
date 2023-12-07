6954cc975b90d401dcacfc540ef1907bf3182114
Tizekod <noon.tizekod@gmail.com>
2023-12-07 00:08:09 -0500

    Fixes the string input

--- cci/urlify/urlify.spec.js
+++ cci/urlify/urlify.spec.js
@@ -3,6 +3,6 @@ import { urlify } from "./urlify";
 export const urlify_ = 
 describe('URLify', () => {
     test('Should return Mr%20John%20Smith for Mr John Smith', () => {
-        expect(urlify('Mr John Smith    ')).toEqual('Mr%20John%20Smith');
+        expect(urlify('Mr John Smith', 13)).toEqual('Mr%20John%20Smith');
     });
 });
