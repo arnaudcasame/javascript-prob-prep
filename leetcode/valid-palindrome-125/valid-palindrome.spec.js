c67321de744695762fd01b0403be1cc2c2aa492e
Tizekod <noon.tizekod@gmail.com>
2023-12-08 14:12:27 -0500

    Adds test suite for O(n) solution of valid palindrome

--- leetcode/valid-palindrome-125/valid-palindrome.spec.js
+++ leetcode/valid-palindrome-125/valid-palindrome.spec.js
@@ -1,4 +1,4 @@
-import { isPalindrome } from "./valid-palindrome";
+import { isPalindrome, isPalindromeOp } from "./valid-palindrome";
 
 export const isPalindrome_ = 
 describe('Is a Valid Palindrome', function(){
@@ -12,5 +12,17 @@ describe('Is a Valid Palindrome', function(){
 
     test('Should return true for ()', function(){
         expect(isPalindrome(" ")).toEqual(true);
+    });
+
+    test('Should return true for (A man, a plan, a canal: Panama) O(n)', function(){
+        expect(isPalindromeOp("A man, a plan, a canal: Panama")).toEqual(true);
+    });
+    
+    test('Should return true for (race a car) O(n)', function(){
+        expect(isPalindromeOp("race a car")).toEqual(false);
+    });
+
+    test('Should return true for ( ) O(n)', function(){
+        expect(isPalindromeOp(" ")).toEqual(true);
     });
 });
