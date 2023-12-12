955278f0cc8a0d42d2a3942caf7330e42269e914
Tizekod <noon.tizekod@gmail.com>
2023-12-11 22:40:01 -0500

    Implements maximum product solution tests

--- leetcode/maximum-product-two-elements-in-array-1464/maximum-product.spec.js
+++ leetcode/maximum-product-two-elements-in-array-1464/maximum-product.spec.js
@@ -5,4 +5,12 @@ export default describe('Maximum Product of Two Elements in an Array', function(
     test('Should return 12 for [3,4,5,2]', function(){
         expect(maxProduct([3,4,5,2])).toEqual(12);
     });
+
+    test('Should return 16 for [1,5,4,5]', function(){
+        expect(maxProduct([1,5,4,5])).toEqual(16);
+    });
+
+    test('Should return 12 for [3,7]', function(){
+        expect(maxProduct([3,7])).toEqual(12);
+    });
 });
