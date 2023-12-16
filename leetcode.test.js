0e71fa33df3d09d81baf20716ec8f86a6dcf1f10
Tizekod <noon.tizekod@gmail.com>
2023-12-15 21:00:08 -0500

    Adds consecutive characters test suite to main module

--- leetcode.test.js
+++ leetcode.test.js
@@ -22,3 +22,4 @@ import "./leetcode/top-K-frequent-elements-347/top-K-frequent-elements.spec";
 import "./leetcode/product-of-array-except-self-238/product-of-array.spec";
 import "./leetcode/difference-between-ones-zeros-2482/diff-between-ones-zeros.spec";
 import "./leetcode/longest-consecutive-sequence-128/longest-consecutive-sequence.spec";
+import "./leetcode/consecutive-characters-1446/consecutive-characters.spec";
