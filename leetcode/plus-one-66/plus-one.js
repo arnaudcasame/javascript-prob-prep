9979ed64b6c1efa75c56fc91a74d14ffb7b2516f
Tizekod <noon.tizekod@gmail.com>
2023-12-18 02:07:13 -0500

    Uses spread operator instead of array unshift method

--- leetcode/plus-one-66/plus-one.js
+++ leetcode/plus-one-66/plus-one.js
@@ -4,15 +4,15 @@
  */
 export const plusOne = function(digits) {
     let rem = 1;
-    const res = []
+    let res = []
     for (let i = 0; i < digits.length; i++) {
         const indice = digits.length -1 - i;
         const curr = (digits[indice] + rem) % 10;
         rem = Math.floor((digits[indice] + rem) / 10);
         if(i === (digits.length-1) && rem > 0){
-            res.unshift(rem, 0)
+            res = [rem, 0, ...res] // res.unshift(rem, 0)
         } else {
-            res.unshift(curr);
+            res = [curr, ...res]; // res.unshift(curr)
         }
     }
     return res;
