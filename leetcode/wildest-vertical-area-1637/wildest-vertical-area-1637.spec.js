bb964d0ba527d9fc2d281d7b3d2e9e0123832c03
Tizekod <noon.tizekod@gmail.com>
2023-12-22 04:03:07 -0500

    Unskips one of the tests

--- leetcode/wildest-vertical-area-1637/wildest-vertical-area-1637.spec.js
+++ leetcode/wildest-vertical-area-1637/wildest-vertical-area-1637.spec.js
@@ -1,7 +1,7 @@
 import { maxWidthOfVerticalArea } from "./wildest-vertical-area-1637";
 
 export default describe('Wildest Vertical Area between two Points containing no Points', function(){
-    xtest('Shoudl return 1 for ', function(){
+    test('Shoudl return 1 for ', function(){
         expect(maxWidthOfVerticalArea([[8,7],[9,9],[7,4],[9,7]])).toEqual(1);
     });
 
