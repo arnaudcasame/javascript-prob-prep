2e08a72213745d7bed75cc111b66af64cdf1b8e3
Tizekod <noon.tizekod@gmail.com>
2023-12-23 15:37:18 -0500

    Emptied the last solution

--- cci/remove-duplicates-linked-list-2.1/remove-dups.js
+++ cci/remove-duplicates-linked-list-2.1/remove-dups.js
@@ -1,17 +1,6 @@
 import { LinkedList } from "../../data-structures/linked-list";
 
-export const removeDups = function(list){
-    const link = LinkedList.from(list);
-    const s = new Set();
-    while(link.next()){
-        if(s.has(link.current.val)){
-            link.removeNode(link.current.val);
-        }else{
-            s.add(link.current.val)
-        }
-    }
-    return LinkedList.toArray(link);
-};
+export const removeDups = function(list){};
 
 // No extra buffer to solve it
 export const removeDupsNoBuffer = function(list){};
