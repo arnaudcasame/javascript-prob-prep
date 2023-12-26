8be8a520934cd4e451d18087fedf90edbabefa61
Tizekod <noon.tizekod@gmail.com>
2023-12-25 22:07:02 -0500

    Adds Kth to last CCI problem

---
+++ cci/linked-list/2.2-kth-to-last/kth-to-last.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the Kth element to Last of the array
+ * @param {number[]} arr 
+ * @return {number[]}
+ */
+export const kthToLast = function(arr){
+    return arr
+}
---
+++ cci/linked-list/2.2-kth-to-last/kth-to-last.spec.js
@@ -0,0 +1,15 @@
+import { kthToLast } from "./kth-to-last";
+
+export default describe('Return Kth to Last', function(){
+    xtest('Should return ', function(){
+        expect(kthToLast([])).toEqual([]);
+    });
+
+    xtest('Should return ', function(){
+        expect(kthToLast([])).toEqual([]);
+    });
+
+    xtest('Should return ', function(){
+        expect(kthToLast([])).toEqual([]);
+    });
+});
