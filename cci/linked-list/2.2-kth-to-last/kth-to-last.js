3364c5233f4adad103dbe27dcf12cb7056edb1a4
Tizekod <noon.tizekod@gmail.com>
2023-12-26 02:20:20 -0500

    Wait

--- cci/linked-list/2.2-kth-to-last/kth-to-last.js
+++ cci/linked-list/2.2-kth-to-last/kth-to-last.js
@@ -1,8 +1,20 @@
-/**
+import { ListNode } from "../../../data-structures/list-node";
+/** 
  * Returns the Kth element to Last of the array
  * @param {number[]} arr 
  * @return {number[]}
  */
-export const kthToLast = function(arr){
+export const kthToLast = function(arr, kth){
+    let head;
+    let current;
+    for (const num of arr) {
+        if(!head){
+            head = new ListNode(num);
+            current = head;
+        }else{
+            current.next = new ListNode(num);
+        }
+    }
+    console.log(current)
     return arr
 }
--- cci/linked-list/2.2-kth-to-last/kth-to-last.spec.js
+++ cci/linked-list/2.2-kth-to-last/kth-to-last.spec.js
@@ -1,8 +1,8 @@
 import { kthToLast } from "./kth-to-last";
 
 export default describe('Return Kth to Last', function(){
-    xtest('Should return ', function(){
-        expect(kthToLast([])).toEqual([]);
+    test('Should return ', function(){
+        expect(kthToLast([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])).toEqual([10,11,12,13,14,15]);
     });
 
     xtest('Should return ', function(){
