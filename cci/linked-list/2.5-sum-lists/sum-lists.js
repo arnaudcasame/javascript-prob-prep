5c1579e01be00cd7e48cd6cc3bab7205565fb711
Tizekod <noon.tizekod@gmail.com>
2023-12-28 01:53:03 -0500

    Solves the sum list problem

--- cci/linked-list/2.5-sum-lists/sum-lists.js
+++ cci/linked-list/2.5-sum-lists/sum-lists.js
@@ -1,3 +1,22 @@
-export const sumLists = function(arr1, arr2){
+import { ListNode } from "../../../data-structures/list-node";
 
+export const sumLists = function(list1, list2){
+    let remain = 0, head, curr;
+    while(list1 && list2){
+        if(!head){
+            head = new ListNode(((list1.val+list2.val+remain)%10));
+            curr = head;
+        }else{
+            curr.next = new ListNode(((list1.val+list2.val+remain)%10))
+            curr = curr.next;
+        }
+        remain = Math.floor((list1.val+list2.val+remain)/10);
+        list1 = list1.next;
+        list2 = list2.next;
+    }
+    if(remain){
+        curr.next = new ListNode(remain);
+        curr = curr.next;
+    }
+    return head.toString()
 }
--- cci/linked-list/2.5-sum-lists/sum-lists.spec.js
+++ cci/linked-list/2.5-sum-lists/sum-lists.spec.js
@@ -1,7 +1,16 @@
 import { sumLists } from "./sum-lists";
+import { ListNode } from "../../../data-structures/list-node";
 
 export default describe('Sum Lists (Linked-List)', function(){
-    xtest('title', function(){
-        expect(sumLists([], [])).toEqual(0);
+    test('Should Return (9)(1)(2)', function(){
+        const list1 = ListNode.fromArray([7,1,6]);
+        const list2 = ListNode.fromArray([5,9,2])
+        expect(sumLists(list1, list2)).toEqual('(2)(1)(9)');
+    });
+
+    test('Should Return (2)(1)(1)(1)', function(){
+        const list1 = ListNode.fromArray([7,1,6]);
+        const list2 = ListNode.fromArray([5,9,4])
+        expect(sumLists(list1, list2)).toEqual('(2)(1)(1)(1)');
     });
 });
