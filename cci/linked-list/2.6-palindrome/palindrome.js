7705d426a9b8b8c0eac9b77300ad5e5b74d17555
Tizekod <noon.tizekod@gmail.com>
2023-12-28 23:11:58 -0500

    Solves the LinkedList Palindrome problem

--- cci/linked-list/2.6-palindrome/palindrome.js
+++ cci/linked-list/2.6-palindrome/palindrome.js
@@ -1,3 +1,23 @@
-export const palindrome = function(arr){
-    return true;
+import { Stack } from "../../../data-structures/stack";
+export const palindrome = function(list, length){
+    const stack = new Stack();
+    const mid = Math.floor(length/2) + length%2;
+    let i = 1;
+    while(list){
+        if((i < mid && length%2 !== 0) || (i <= mid && length%2 === 0)){
+            stack.push(list.val);
+            console.log(i, mid, stack.data_)
+            i++;
+        }else if(i===mid && mid%2 !== 0){
+            // Skipping unique value
+            i++;
+        }else{
+            const curr = stack.pop();
+            if(curr !== list.val){
+                stack.push(curr)
+            }
+        }
+        list = list.next
+    }
+    return stack.isEmpty();
 }
