836eb30e32ba64f470b137a4aefd3961e94c5c17
Tizekod <noon.tizekod@gmail.com>
2023-12-28 19:22:10 -0500

    Solves the Sum Lists problem in Forward order

--- cci/linked-list/2.5-sum-lists/sum-lists.js
+++ cci/linked-list/2.5-sum-lists/sum-lists.js
@@ -20,3 +20,22 @@ export const sumLists = function(list1, list2){
     }
     return head.toString()
 }
+
+export const sumListsForward = function(list1, list2){
+    let remain = 0, head, curr, pre;
+    while(list1 && list2){
+        remain = Math.floor((list1.val + list2.val + remain)/10);
+        if(!head){
+            head = remain ? new ListNode(remain, new ListNode((list1.val + list2.val)%10)) : new ListNode((list1.val + list2.val)%10);
+            curr = head.next ? head.next : head;
+        }else{
+            pre = curr;
+            pre.val = pre.val + remain;
+            curr.next = new ListNode((list1.val + list2.val)%10);
+            curr = curr.next;
+        }
+        list1 = list1.next;
+        list2 = list2.next;
+    }
+    return head.toString()
+}
--- cci/linked-list/2.5-sum-lists/sum-lists.spec.js
+++ cci/linked-list/2.5-sum-lists/sum-lists.spec.js
@@ -1,8 +1,8 @@
-import { sumLists } from "./sum-lists";
+import { sumLists, sumListsForward } from "./sum-lists";
 import { ListNode } from "../../../data-structures/list-node";
 
 export default describe('Sum Lists (Linked-List)', function(){
-    test('Should Return (9)(1)(2)', function(){
+    test('Should Return (2)(1)(9)', function(){
         const list1 = ListNode.fromArray([7,1,6]);
         const list2 = ListNode.fromArray([5,9,2])
         expect(sumLists(list1, list2)).toEqual('(2)(1)(9)');
@@ -12,5 +12,18 @@ export default describe('Sum Lists (Linked-List)', function(){
         const list1 = ListNode.fromArray([7,1,6]);
         const list2 = ListNode.fromArray([5,9,4])
         expect(sumLists(list1, list2)).toEqual('(2)(1)(1)(1)');
+    });
+
+    // Follow Up test with the digits stored in FORWARD-ORDER
+    test('Should Return (9)(1)(2) [Forward-Order]', function(){
+        const list1 = ListNode.fromArray([6,1,7]);
+        const list2 = ListNode.fromArray([2,9,5])
+        expect(sumListsForward(list1, list2)).toEqual('(9)(1)(2)');
+    });
+
+    test('Should Return (1)(1)(1)(2) [Forward-Order]', function(){
+        const list1 = ListNode.fromArray([6,1,7]);
+        const list2 = ListNode.fromArray([4,9,5]);
+        expect(sumListsForward(list1, list2)).toEqual('(1)(1)(1)(2)');
     });
 });
