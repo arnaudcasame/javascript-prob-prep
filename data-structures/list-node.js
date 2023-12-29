89c8d878056bc2ff4679029285f0e501f4c1e6d0
Tizekod <noon.tizekod@gmail.com>
2023-12-28 20:06:53 -0500

    Implements a few helper functions in ListNode

--- data-structures/list-node.js
+++ data-structures/list-node.js
@@ -2,3 +2,35 @@ export function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
 }
+
+ListNode.prototype.toString = function(){
+    let result = '';
+    let head = this;
+    while(head){
+        result += `(${head.val})`;
+        head = head.next;
+    }
+    return result;
+}
+
+ListNode.toArray =  function(listNode){
+    const arr = [];
+    while(listNode){
+        arr.push(listNode.val ?? listNode.data);
+    }
+}
+
+ListNode.fromArray = function(arr){
+    let head;
+    let curr;
+    for (const item of arr) {
+        if(!head){
+            head = new ListNode(item);
+            curr = head;
+            continue;
+        }
+        curr.next = new ListNode(item);
+        curr = curr.next;
+    }
+    return head;
+}
