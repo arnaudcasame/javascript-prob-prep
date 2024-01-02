b69475ffad06faac4e743a74f25aa61e88a8e659
Tizekod <noon.tizekod@gmail.com>
2024-01-02 13:50:10 -0500

    Prepares list-nodes for test

--- cci/linked-list/2.7-intersection/intersection.spec.js
+++ cci/linked-list/2.7-intersection/intersection.spec.js
@@ -1,7 +1,10 @@
-import { intersect  } from "./intersection.js";
-
-export default describe('Intersection of 2 LinkedLists', function(){
-    xit('title', function(){
-        expect(intersect([], [])).to.equal(0);
-    });
+import {intersect} from './intersection.js';
+import {ListNode} from '../../../data-structures/list-node.js';
+export default describe('Intersection of 2 LinkedLists', function() {
+  xit('title', function() {
+    const list1 = ListNode.fromArray([]);
+    const list2 = ListNode.fromArray([]);
+    // eslint-disable-next-line no-undef
+    expect(intersect(list1, list2)).to.equal(0);
+  });
 });
