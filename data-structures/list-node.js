2ceba714ff1b70f5a4f8405702c105c825b95d2b
Tizekod <noon.tizekod@gmail.com>
2024-01-02 14:35:08 -0500

    Implements link two lists function

--- data-structures/list-node.js
+++ data-structures/list-node.js
@@ -20,7 +20,7 @@ ListNode.toArray =  function(listNode){
     while(listNode){
         arr.push(listNode.val ?? listNode.data);
     }
-}
+};
 
 ListNode.fromArray = function(arr){
     let head;
@@ -35,4 +35,40 @@ ListNode.fromArray = function(arr){
         curr = curr.next;
     }
     return head;
-}
+};
+
+ListNode.link2Lists = function(arr1, arr2, pos1, pos2){
+    let head1, curr1, head2, curr2, i=0, j=0;
+    while (i < arr1.length && j < arr2.length) {
+        if(!head1 || !head2){
+            head1 = new ListNode(arr1[i]);
+            curr1 = head1;
+
+            head2 = new ListNode(arr2[j]);
+            curr2 = head2;
+            i++;
+            j++;
+            continue;
+        }else if(i===pos1 && j===pos2 && arr1[i] === arr2[j]){
+            curr1.next = new ListNode(arr1[i]);
+            curr2.next = curr1.next;
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+            i++;
+            j++;
+            continue;
+        }
+        
+        if(pos1 !== i){
+            curr1.next = new ListNode(arr1[i]);
+            curr1 = curr1.next;
+            i++;
+        }
+        if(pos2 !== j){
+            curr2.next = new ListNode(arr2[j]);
+            curr2 = curr2.next;
+            j++;
+        }
+    }
+    return [head1, head2];
+};
