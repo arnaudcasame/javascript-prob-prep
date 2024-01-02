c7b134baebc5c157e1290803a0abf7488c9f95d2
Tizekod <noon.tizekod@gmail.com>
2024-01-02 01:42:14 -0500

    Fixes import statements to support the no-types environment

--- cci.test.js
+++ cci.test.js
@@ -1,18 +1,16 @@
-import console from "console"
-global.console = console
-import { is_unique } from "./cci/is-unique/is-unique.spec";
-import { CheckPermutation } from "./cci/check-permutation/check-permutation.spec";
-import { common } from "./cci/common-set-members/common-set-members.spec";
-import { urlify_ } from "./cci/urlify/urlify.spec";
-import { isPermutatedPalindrome_ } from "./cci/palindrome-permutation/palindrome-permutation.spec";
-import "./cci/one-away/one-away.spec";
-import "./cci/string-compression/string-compressiion.spec";
-import "./cci/string-rotation/string-rotation.spec";
-import './cci/remove-duplicates-linked-list-2.1/remove-dups.spec';
-import './cci/linked-list/2.2-kth-to-last/kth-to-last.spec';
-import "./cci/linked-list/2.3-delete-middle-node/delete-middle-node.spec";
-import './cci/linked-list/2.4-partition/partition.spec';
-import './cci/linked-list/2.5-sum-lists/sum-lists.spec';
-import './cci/linked-list/2.6-palindrome/palindrome.spec';
-import './cci/linked-list/2.7-intersection/intersection.spec';
-import './cci/linked-list/2.8-loop-detection/loop-detection.spec';
+import "./cci/is-unique/is-unique.spec.js";
+import "./cci/check-permutation/check-permutation.spec.js";
+import "./cci/common-set-members/common-set-members.spec.js";
+import "./cci/urlify/urlify.spec.js";
+import "./cci/palindrome-permutation/palindrome-permutation.spec.js";
+import "./cci/one-away/one-away.spec.js";
+import "./cci/string-compression/string-compressiion.spec.js";
+import "./cci/string-rotation/string-rotation.spec.js";
+import './cci/remove-duplicates-linked-list-2.1/remove-dups.spec.js';
+import './cci/linked-list/2.2-kth-to-last/kth-to-last.spec.js';
+import "./cci/linked-list/2.3-delete-middle-node/delete-middle-node.spec.js";
+import './cci/linked-list/2.4-partition/partition.spec.js';
+import './cci/linked-list/2.5-sum-lists/sum-lists.spec.js';
+import './cci/linked-list/2.6-palindrome/palindrome.spec.js';
+import './cci/linked-list/2.7-intersection/intersection.spec.js';
+import './cci/linked-list/2.8-loop-detection/loop-detection.spec.js';
