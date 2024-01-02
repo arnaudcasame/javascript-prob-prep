3acee851907132546dc959bf3a4e6dc39e1aa461
Tizekod <noon.tizekod@gmail.com>
2024-01-02 02:26:21 -0500

    Removes logging statements

--- cci/linked-list/2.6-palindrome/palindrome.js
+++ cci/linked-list/2.6-palindrome/palindrome.js
@@ -6,7 +6,7 @@ export const palindrome = function(list, length){
     while(list){
         if((i < mid && length%2 !== 0) || (i <= mid && length%2 === 0)){
             stack.push(list.val);
-            console.log(i, mid, stack.data_)
+            // console.log(i, mid, stack.data_)
             i++;
         }else if(i===mid && mid%2 !== 0){
             // Skipping unique value
