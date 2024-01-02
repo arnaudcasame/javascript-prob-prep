0581ec1d3715dc8ae42b23ed875eb59069330336
Tizekod <noon.tizekod@gmail.com>
2024-01-02 15:41:26 -0500

    Uses the link2Lists method for testing purposes

--- cci/linked-list/2.7-intersection/intersection.spec.js
+++ cci/linked-list/2.7-intersection/intersection.spec.js
@@ -1,10 +1,12 @@
 import {intersect} from './intersection.js';
 import {ListNode} from '../../../data-structures/list-node.js';
 export default describe('Intersection of 2 LinkedLists', function() {
-  xit('title', function() {
-    const list1 = ListNode.fromArray([]);
-    const list2 = ListNode.fromArray([]);
-    // eslint-disable-next-line no-undef
-    expect(intersect(list1, list2)).to.equal(0);
-  });
+    it('Should return the intersecting node', function() {
+        const links = ListNode.link2Lists([3, 1, 5, 9, 7, 2, 1],
+            [4, 6, 7, 2, 1], 4, 2);
+        const list1 = links[0];
+        const list2 = links[1];
+        // eslint-disable-next-line no-undef
+        expect(intersect(list1, list2)).to.equal(0);
+    });
 });
