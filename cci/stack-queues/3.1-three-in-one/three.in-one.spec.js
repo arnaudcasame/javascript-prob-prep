829b93060fd064753edcc693d06fc938d7cb9505
Tizekod <noon.tizekod@gmail.com>
2024-01-02 22:23:36 -0500

    Adds the three in one problem

---
+++ cci/stack-queues/3.1-three-in-one/three.in-one.spec.js
@@ -0,0 +1,8 @@
+import {threeInOne} from './three-in-one';
+
+export default describe('title', function() {
+    xit('title', function() {
+        // eslint-disable-next-line no-undef
+        expect(threeInOne()).to.equal();
+    });
+});
