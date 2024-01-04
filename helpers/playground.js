b6410950c810ab219bf35afbe915879dd82863f4
Tizekod <noon.tizekod@gmail.com>
2024-01-03 22:59:27 -0500

    Uses QueueLL object to test it

--- helpers/playground.js
+++ helpers/playground.js
@@ -1,4 +1,5 @@
 import {StackLL} from '../data-structures/stack.js';
+import {QueueLL} from '../data-structures/queue.js';
 
 
 // Creating a StackLL object
@@ -23,3 +24,25 @@ console.log('#StackLL: Pop', stackLL.pop().toString());
 // Check if StackLL isEmpty
 console.log('#StackLL is EMpty:', stackLL.isEmpty());
 
+
+// Creating a queueLL object
+const queueLL = new QueueLL();
+// enqueue item onto the queueLL
+queueLL.enqueue(1);
+queueLL.enqueue(2);
+queueLL.enqueue(3);
+// Remove item from to of queueLL
+console.log('\n#QueueLL: enqueue', queueLL.peek().toString());
+console.log('#QueueLL: dequeue', queueLL.dequeue().toString());
+console.log('#QueueLL: Print', queueLL.peek().toString());
+// enqueue item onto the queueLL
+console.log('#QueueLL: dequeue', queueLL.dequeue().toString());
+queueLL.enqueue(4);
+console.log('#QueueLL: enqueue', queueLL.peek().toString());
+// Check if queueLL isEmpty
+console.log('#QueueLL is EMpty:', queueLL.isEmpty());
+// dequeue to empty queueLL
+console.log('#QueueLL: dequeue', queueLL.dequeue().toString());
+console.log('#QueueLL: dequeue', queueLL.dequeue().toString());
+// Check if queueLL isEmpty
+console.log('#QueueLL is EMpty:', queueLL.isEmpty());
