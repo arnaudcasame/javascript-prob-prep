a57c2c3287d5bc7aaea1cab641fb3c540b36a757
Tizekod <noon.tizekod@gmail.com>
2024-02-11 01:06:02 -0500

    Adds Cracking the Coding interview problem 3.4

---
+++ cci/queue-via-stacks-3.4/my-queue.js
@@ -0,0 +1,32 @@
+/**
+ * Class that represents a Queue
+ * using two stacks
+ */
+export class MyQueue {
+    /**
+     * places an item at the back of the queue;
+     * @param {T} item item to be placed at the
+     * back of the queue
+     */
+    enqueue(item) {
+
+    }
+
+    /**
+     * Retrieves the item at the front of the queue,
+     * and removes it from the queue;
+     * @return {T} item at the front
+     */
+    dequeue() {
+        return null;
+    }
+
+    /**
+     * retrieves the item at the front of the
+     * queue without removing it from the queue
+     * @return {T}
+     */
+    peek() {
+        return null;
+    }
+}
---
+++ cci/queue-via-stacks-3.4/my-queue.spec.js
@@ -0,0 +1,31 @@
+import {MyQueue} from './my-queue.js';
+import {expect} from 'chai';
+
+export default describe('Queue Via Stacks', function() {
+    let queue;
+    before(function() {
+        queue = new MyQueue();
+        queue.enqueue(1);
+    });
+
+    xit('First peek should equal 1', function() {
+        expect(queue.peek()).to.equal(1);
+        queue.enqueue(2);
+    });
+
+    xit('Second peek should equal 2', function() {
+        expect(queue.peek()).to.equal(2);
+    });
+
+    xit('Popping the Queue should equal 2', function() {
+        expect(queue.dequeue()).to.equal(2);
+    });
+
+    xit('Popping the Queue should equal 1', function() {
+        expect(queue.dequeue()).to.equal(2);
+    });
+
+    xit('At this point the Queue should be empty', function() {
+        expect(queue.empty()).to.be.true;
+    });
+});
