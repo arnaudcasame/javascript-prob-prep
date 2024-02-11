9b42f2d1b43fa06cc9fe54bc5e72fc542e149e16
Tizekod <noon.tizekod@gmail.com>
2024-02-11 02:42:53 -0500

    Prepares  cracking the coding interview stack sorting problem

--- cci/stack-queues/3.5-sort-stack/sort-stack.js
+++ cci/stack-queues/3.5-sort-stack/sort-stack.js
@@ -1,3 +1,11 @@
-export const sortStack = function() {
+import {Stack} from '../../../data-structures/stack.js';
 
+/**
+ * Sorts stack elements such that
+ * the smallest items are on the top
+ * @param {Stack} stack to be sorted
+ * @return {Stack} sorted stack
+ */
+export const sortStack = function(stack) {
+    return new Stack();
 };
--- cci/stack-queues/3.5-sort-stack/sort-stack.spec.js
+++ cci/stack-queues/3.5-sort-stack/sort-stack.spec.js
@@ -1,8 +1,41 @@
 import {sortStack} from './sort-stack.js';
+import {Stack} from '../../../data-structures/stack.js';
+import {expect} from 'chai';
 
 export default describe('#Sort Stack', function() {
-    xit('title', function() {
-        // eslint-disable-next-line no-undef
-        expect(sortStack()).to.equal();
+    let sortedStack;
+    before(function() {
+        const stack = new Stack();
+        stack.push(10);
+        stack.push(3);
+        stack.push(9);
+        stack.push(12);
+        stack.push(8);
+        stack.push(33);
+        sortedStack = sortStack(stack);
+    });
+
+    xit('Should be equal to 3', function() {
+        expect(sortedStack.pop()).to.equal(3);
+    });
+
+    xit('Should be equal to 8', function() {
+        expect(sortedStack.pop()).to.equal(8);
+    });
+
+    xit('Should be equal to 9', function() {
+        expect(sortedStack.pop()).to.equal(9);
+    });
+
+    xit('Should be equal to 10', function() {
+        expect(sortedStack.pop()).to.equal(10);
+    });
+
+    xit('Should be equal to 12', function() {
+        expect(sortedStack.pop()).to.equal(12);
+    });
+
+    xit('Should be equal to 33', function() {
+        expect(sortedStack.pop()).to.equal(33);
     });
 });
