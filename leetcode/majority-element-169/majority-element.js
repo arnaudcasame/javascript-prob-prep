e2b0f46421c22e54670710b00db017664e3bac72
Tizekod <noon.tizekod@gmail.com>
2024-02-12 02:46:56 -0500

    Solves Leetcode problem 169 optimally

--- leetcode/majority-element-169/majority-element.js
+++ leetcode/majority-element-169/majority-element.js
@@ -16,3 +16,15 @@ export const majorityElement = function(nums) {
     }
     return 0;
 };
+
+export const majorityElementO = function(nums) {
+    let majority = 0; let result = 0;
+    for (const num of nums) {
+        if (majority === 0) {
+            result = num;
+        }
+
+        majority += num === result ? 1 : -1;
+    }
+    return result;
+};
--- leetcode/majority-element-169/majority-element.spec.js
+++ leetcode/majority-element-169/majority-element.spec.js
@@ -1,4 +1,4 @@
-import {majorityElement} from './majority-element.js';
+import {majorityElement, majorityElementO} from './majority-element.js';
 import {expect} from 'chai';
 
 export default describe('#169: Majority Element', function() {
@@ -8,5 +8,13 @@ export default describe('#169: Majority Element', function() {
 
     it('Should return 2 for [2,2,1,1,1,2,2]', function() {
         expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).to.equal(2);
+    });
+
+    it('Should return 3 for [3,2,3]', function() {
+        expect(majorityElementO([3, 2, 3])).to.equal(3);
+    });
+
+    it('Should return 2 for [2,2,1,1,1,2,2]', function() {
+        expect(majorityElementO([2, 2, 1, 1, 1, 2, 2])).to.equal(2);
     });
 });
