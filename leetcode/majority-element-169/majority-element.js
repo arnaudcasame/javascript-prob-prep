79b8a65b1cac1e077591fa40c29d0fd39abc9573
Tizekod <noon.tizekod@gmail.com>
2024-02-11 23:54:44 -0500

    Solves the Leetcode problem 169

--- leetcode/majority-element-169/majority-element.js
+++ leetcode/majority-element-169/majority-element.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 export const majorityElement = function(nums) {
+    const bucket = {};
+    for (const num of nums) {
+        bucket[num] = bucket[num] ? bucket[num] + 1 : 1;
+    }
+    // eslint-disable-next-line guard-for-in
+    for (const key in bucket) {
+        const element = bucket[key];
+        if (element > nums.length/2) {
+            return +key;
+        }
+    }
     return 0;
 };
--- leetcode/majority-element-169/majority-element.spec.js
+++ leetcode/majority-element-169/majority-element.spec.js
@@ -2,11 +2,11 @@ import {majorityElement} from './majority-element.js';
 import {expect} from 'chai';
 
 export default describe('#169: Majority Element', function() {
-    xit('Should return 3 for [3,2,3]', function() {
+    it('Should return 3 for [3,2,3]', function() {
         expect(majorityElement([3, 2, 3])).to.equal(3);
     });
 
-    xit('Should return 2 for [2,2,1,1,1,2,2]', function() {
+    it('Should return 2 for [2,2,1,1,1,2,2]', function() {
         expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).to.equal(2);
     });
 });
