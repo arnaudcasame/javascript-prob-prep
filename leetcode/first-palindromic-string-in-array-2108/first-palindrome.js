c5b373d294587288cb77c99c3aebfc7389ce0edc
Tizekod <noon.tizekod@gmail.com>
2024-02-12 22:32:02 -0500

    Solves leetcode problem #2108

--- leetcode/first-palindromic-string-in-array-2108/first-palindrome.js
+++ leetcode/first-palindromic-string-in-array-2108/first-palindrome.js
@@ -3,5 +3,29 @@
  * @return {string}
  */
 export const firstPalindrome = function(words) {
+    for (const word of words) {
+        if (isPalindrome(word)) {
+            return word;
+        }
+    }
     return '';
 };
+
+/**
+ * Determines whether a string
+ * is a Palindrome
+ * @param {string} substr the string to operate on
+ * @return {boolean}
+ */
+function isPalindrome(substr) {
+    const len = substr.length;
+    const n = len%2 == 0 ? (len/2)+1 : Math.round(len/2)+1;
+    for (let i = 0; i < n; i++) {
+        const left = i;
+        const right = len - 1 - i;
+        if (substr.charAt(left) != substr.charAt(right)) {
+            return false;
+        }
+    }
+    return true;
+}
--- leetcode/first-palindromic-string-in-array-2108/first-palindrome.spec.js
+++ leetcode/first-palindromic-string-in-array-2108/first-palindrome.spec.js
@@ -3,15 +3,15 @@ import {firstPalindrome} from './first-palindrome.js';
 import {expect} from 'chai';
 
 export default describe('#2108: Find First Palindromic String in the Array', function() {
-    xit('Should return `ada` for input ["abc", "car", "ada", "racecar", "cool"]', function() {
+    it('Should return `ada` for input ["abc", "car", "ada", "racecar", "cool"]', function() {
         expect(firstPalindrome(['abc', 'car', 'ada', 'racecar', 'cool'])).to.equal('ada');
     });
 
-    xit('Should return `racecar` for input ["notapalindrome","racecar"]', function() {
+    it('Should return `racecar` for input ["notapalindrome","racecar"]', function() {
         expect(firstPalindrome(['notapalindrome', 'racecar'])).to.equal('racecar');
     });
 
-    xit('Should return empty string for input ["def","ghi"]', function() {
+    it('Should return empty string for input ["def","ghi"]', function() {
         expect(firstPalindrome(['def', 'ghi'])).to.equal('');
     });
 });
