781c5774e4ba77806ffe104121c52a7acb7a7122
Tizekod <noon.tizekod@gmail.com>
2024-02-14 01:06:13 -0500

    Fixes typo in test name & description

--- leetcode/rearrange-array-elements-by-sign-2149/rearrange-array.spec.js
+++ leetcode/rearrange-array-elements-by-sign-2149/rearrange-array.spec.js
@@ -3,7 +3,7 @@ import {rearrangeArray} from './rearrange-array.js';
 import {expect} from 'chai';
 
 export default describe('#2149: Rearrange Array Elements by Sign', function() {
-    xit('Should return [3,1,-2,-5,2,-4] for input = []', function() {
+    xit('Should return [3,-2,1,-5,2,-4] for input = [3,1,-2,-5,2,-4]', function() {
         expect(rearrangeArray([3, 1, -2, -5, 2, -4])).to.equal([3, -2, 1, -5, 2, -4]);
     });
 
