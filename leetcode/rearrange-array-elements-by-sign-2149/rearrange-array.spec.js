58cdcf6c84ebc72a0477b6bc65a3cda2d87d3908
Tizekod <noon.tizekod@gmail.com>
2024-02-14 01:04:52 -0500

    Adds leetcode problem #2149

---
+++ leetcode/rearrange-array-elements-by-sign-2149/rearrange-array.spec.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import {rearrangeArray} from './rearrange-array.js';
+import {expect} from 'chai';
+
+export default describe('#2149: Rearrange Array Elements by Sign', function() {
+    xit('Should return [3,1,-2,-5,2,-4] for input = []', function() {
+        expect(rearrangeArray([3, 1, -2, -5, 2, -4])).to.equal([3, -2, 1, -5, 2, -4]);
+    });
+
+    xit('Should return [1,-1] for input = [-1,1]', function() {
+        expect(rearrangeArray([-1, 1])).to.equal([1, -1]);
+    });
+});
