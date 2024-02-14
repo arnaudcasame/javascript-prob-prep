c2ee9d2b12e2c72a72d24f9c056ad916dd18b77d
Tizekod <noon.tizekod@gmail.com>
2024-02-14 04:34:10 -0500

    Adds unit test to suite

--- leetcode/sequential-digits-1291/sequential-digits.spec.js
+++ leetcode/sequential-digits-1291/sequential-digits.spec.js
@@ -10,4 +10,8 @@ export default describe('#1291: Sequential Digits', function() {
     xit('Should return [] for input: (1000, 13000)', function() {
         expect(sequentialDigits(1000, 13000)).to.deep.equal([1234, 2345, 3456, 4567, 5678, 6789, 12345]);
     });
+
+    xit('Should return [] for input: (10, 1000000000)', function() {
+        expect(sequentialDigits(10, 1000000000)).to.deep.equal([1234, 2345, 3456, 4567, 5678, 6789, 12345]);
+    });
 });
