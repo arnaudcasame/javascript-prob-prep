f6365b8af2659db7b1c35fcb3504d5a98869c3ad
Tizekod <noon.tizekod@gmail.com>
2024-02-14 01:15:11 -0500

    Solves leetcode problem #2149

--- leetcode/rearrange-array-elements-by-sign-2149/rearrange-array.js
+++ leetcode/rearrange-array-elements-by-sign-2149/rearrange-array.js
@@ -3,5 +3,17 @@
  * @return {number[]}
  */
 export const rearrangeArray = function(nums) {
-    return [];
+    const bucket = new Array(nums.length);
+    let [p, n] = [0, 1];
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (num < 0) {
+            bucket[n] = num;
+            n += 2;
+        } else {
+            bucket[p] = num;
+            p += 2;
+        }
+    }
+    return bucket;
 };
