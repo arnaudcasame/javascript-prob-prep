e1ed133369c1ed8b5bdf883dafe5ab12844403ea
Tizekod <noon.tizekod@gmail.com>
2024-02-14 21:57:50 -0500

    Solves leetcode problem #1291

--- leetcode/sequential-digits-1291/sequential-digits.js
+++ leetcode/sequential-digits-1291/sequential-digits.js
@@ -4,5 +4,73 @@
  * @return {number[]}
  */
 export const sequentialDigits = function(low, high) {
-    return [];
+    const bucket = [];
+    const blueprint = '123456789';
+
+    for (let i = 0; i < blueprint.length; i++) {
+        for (let j = i + 1; j < blueprint.length; j++) {
+            const num = parseInt(blueprint.substring(i, j + 1));
+
+            if (num > high) break;
+            if (low <= num) bucket.push(num);
+        }
+    }
+    return bucket.sort((a, b) => a - b);
 };
+
+/**
+ * My Solution (incomplete)
+ */
+// const bucket = [];
+//     let starting = parseInt((''+low)[0]);
+//     let numDigit = `${low}`.length;
+//     const hiNumDigit = `${high}`.length;
+
+//     let num = `${starting}`;
+//     let j = starting;
+//     let constant = '1';
+//     while (num.length < numDigit) {
+//         if (j+1 === 10) {
+//             starting = 1;
+//             j = starting;
+//             constant = '1';
+//             num = `${starting}`;
+//             numDigit++;
+//             continue;
+//         } else {
+//             num += j+1;
+//             constant += '1';
+//         }
+//         j++;
+//     }
+//     bucket.push(parseInt(num));
+
+//     while (`${bucket[bucket.length-1]}`.length <= hiNumDigit) {
+//         const latest = bucket[bucket.length-1];
+//         if ((''+latest).charAt((''+latest).length-1) === '9') {
+//             constant = ''+((parseInt(constant) * 10) + 1);
+//             let tmp = '';
+//             for (let i = 1; i <= constant.length; i++) {
+//                 tmp += `${i}`;
+//             }
+//             if (high > parseInt(tmp)) {
+//                 bucket.push(parseInt(tmp));
+//                 continue;
+//             } else {
+//                 break;
+//             }
+//         }
+//         const newNum = latest + parseInt(constant);
+//         if (newNum <= high) {
+//             bucket.push(newNum);
+//         } else {
+//             break;
+//         }
+//     }
+
+//     if (bucket[0] < low) {
+//         bucket.splice(0, 1);
+//     }
+
+//     console.log(bucket);
+//     return bucket;
--- leetcode/sequential-digits-1291/sequential-digits.spec.js
+++ leetcode/sequential-digits-1291/sequential-digits.spec.js
@@ -3,11 +3,27 @@ import {sequentialDigits} from './sequential-digits.js';
 import {expect} from 'chai';
 
 export default describe('#1291: Sequential Digits', function() {
-    xit('Should return [] for input: (100, 300)', function() {
+    it('Should return [] for input: (100, 300)', function() {
         expect(sequentialDigits(100, 300)).to.deep.equal([123, 234]);
     });
 
-    xit('Should return [] for input: (1000, 13000)', function() {
+    it('Should return [] for input: (1000, 13000)', function() {
         expect(sequentialDigits(1000, 13000)).to.deep.equal([1234, 2345, 3456, 4567, 5678, 6789, 12345]);
+    });
+
+    it('Should return [] for input: (10, 1000000000)', function() {
+        expect(sequentialDigits(10, 1000000000)).to.deep.equal([12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789]);
+    });
+
+    it('Should return [] for input: (58, 155)', function() {
+        expect(sequentialDigits(58, 155)).to.deep.equal([67, 78, 89, 123]);
+    });
+
+    it('Should return [] for input: (8511, 23553)', function() {
+        expect(sequentialDigits(8511, 23553)).to.deep.equal([12345, 23456]);
+    });
+
+    it('Should return [] for input: (372591426, 841554424)', function() {
+        expect(sequentialDigits(372591426, 841554424)).to.deep.equal([]);
     });
 });
