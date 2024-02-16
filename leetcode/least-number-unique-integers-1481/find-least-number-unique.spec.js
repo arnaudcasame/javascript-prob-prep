2a4a9b2d99c85c30b223d1cd318a9a23d0932f47
Tizekod <noon.tizekod@gmail.com>
2024-02-16 04:37:30 -0500

    Solves leetcode problem #1481

--- leetcode/least-number-unique-integers-1481/find-least-number-unique.spec.js
+++ leetcode/least-number-unique-integers-1481/find-least-number-unique.spec.js
@@ -3,23 +3,23 @@ import {findLeastNumOfUniqueInts} from './find-least-number-unique.js';
 import {expect} from 'chai';
 
 export default describe('#1481: Find Least Number of Unique Integers after K removals', function() {
-    xit('Should return 1 for input: [5,5,4]', function() {
+    it('Should return 1 for input: [5,5,4]', function() {
         expect(findLeastNumOfUniqueInts([5, 5, 4], 1)).to.equal(1);
     });
 
-    xit('Should return 2 for input: [4,3,1,1,3,3,2]', function() {
+    it('Should return 2 for input: [4,3,1,1,3,3,2]', function() {
         expect(findLeastNumOfUniqueInts([4, 3, 1, 1, 3, 3, 2], 2)).to.equal(2);
     });
 
-    xit('Should return 0 for input: [1]', function() {
+    it('Should return 0 for input: [1]', function() {
         expect(findLeastNumOfUniqueInts([1], 1)).to.equal(0);
     });
 
-    xit('Should return 1 for input: [2,1,1,3,3,3]', function() {
+    it('Should return 1 for input: [2,1,1,3,3,3]', function() {
         expect(findLeastNumOfUniqueInts([2, 1, 1, 3, 3, 3], 3)).to.equal(1);
     });
 
-    xit('Should return 3 for input: [2,4,1,8,3,5,1,3]', function() {
+    it('Should return 3 for input: [2,4,1,8,3,5,1,3]', function() {
         expect(findLeastNumOfUniqueInts([2, 4, 1, 8, 3, 5, 1, 3], 3)).to.equal(3);
     });
 });
