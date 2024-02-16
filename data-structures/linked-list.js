015ea14b39ad59dad9e4516d13215c8aedec287d
Tizekod <noon.tizekod@gmail.com>
2024-02-15 23:11:44 -0500

    Refactors the linked list structure

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -1,6 +1,10 @@
-import { ListNode as Node } from "./list-node.js";
+import {ListNode as Node} from './list-node.js';
 
-export function LinkedList(){
+/**
+ * A class representing
+ * the linked list structure
+ */
+export function LinkedList() {
     this.head = null;
     this.tail = null;
     this.current = null;
@@ -10,64 +14,63 @@ export function LinkedList(){
  * Wraps value in Node and inserts it in head
  * @param {Object} v
  */
-LinkedList.prototype.insertNode = function(v){
+LinkedList.prototype.insertNode = function(v) {
     const n = new Node(v);
-    if(!this.head){
+    if (!this.head) {
         this.tail = n;
         this.head = n;
-
-    }else{
+    } else {
         this.tail.next = n;
         this.tail = n;
     }
-}
+};
 
 /**
  * Traverses LinkedList and prints each value in the console;
  */
-LinkedList.prototype.print = function(){
+LinkedList.prototype.print = function() {
     let current = this.head;
-    while(current !== null){
+    while (current !== null) {
         console.log(current.val);
         current = current.next;
     }
-}
+};
 
 /**
  * Removes node with value v from LinkedList
- * @param {Object} v 
+ * @param {Object} v
  */
-LinkedList.prototype.removeNode = function(v){
+LinkedList.prototype.removeNode = function(v) {
     let current = this.head;
-    while(current.next.val !== v){
+    while (current.next.val !== v) {
         current = current.next;
     }
     current.next = current.next.next;
-}
+};
 
-LinkedList.prototype.next = function(){
-    if(!this.current){
+LinkedList.prototype.next = function() {
+    if (!this.current) {
         this.current = this.head;
-    }else{
+    } else {
         this.current = this.current.next;
     }
     return this.current;
 };
 
-LinkedList.from = function(array){
+LinkedList.from = function(array) {
     const link = new LinkedList();
     for (const item of array) {
         link.insertNode(item);
     }
     return link;
-}
+};
 
-LinkedList.toArray = function(link){
-    const arr = new Array();
-    let curr = link.head
-    while(curr !== null){
+LinkedList.toArray = function(link) {
+    const arr = [];
+    let curr = link.head;
+    while (curr !== null) {
         arr.push(curr.val);
         curr = curr.next;
     }
     return arr;
-}
+};
