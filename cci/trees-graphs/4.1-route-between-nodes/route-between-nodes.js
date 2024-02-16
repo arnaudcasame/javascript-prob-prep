82fd1fafd42b198aaf273ac8fff88e238ab74b64
Tizekod <noon.tizekod@gmail.com>
2024-02-15 19:15:36 -0500

    Adds cracking the coding interview problem #4.1

---
+++ cci/trees-graphs/4.1-route-between-nodes/route-between-nodes.js
@@ -0,0 +1,10 @@
+/**
+ * Returns true if there's route between
+ * the source and the destination
+ * @param {Node} source The originating node
+ * @param {Node} destination the destination node
+ * @return {boolean}
+ */
+export const routeBetweenNodes = function(source, destination) {
+    return false;
+};
