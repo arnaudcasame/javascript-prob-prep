c3c9604946136273677836a1e537e066dcca3383
Tizekod <noon.tizekod@gmail.com>
2024-02-15 23:38:11 -0500

    Adds cracking the coding interview problem #4.2

---
+++ cci/trees-graphs/4.2-minimal-tree/minimal-tree.js
@@ -0,0 +1,10 @@
+import {BinarySearchTree} from '../../../data-structures/binary-search-tree.js';
+/**
+ *
+ * @param {number[]} arr -
+ * @return {BinarySearchTree} -
+ */
+export const minimalTree = function(arr) {
+    const bst = new BinarySearchTree(0);
+    return bst;
+};
