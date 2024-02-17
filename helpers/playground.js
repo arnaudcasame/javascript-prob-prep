fcb68c644ef8ba97e891752cd8e49b02dc389326
Tizekod <noon.tizekod@gmail.com>
2024-02-17 02:51:42 -0500

    Updates stack code according to new method names in DS

--- helpers/playground.js
+++ helpers/playground.js
@@ -9,13 +9,13 @@ stackLL.push(1);
 stackLL.push(2);
 stackLL.push(3);
 // Remove item from to of StackLL
-console.log('#StackLL: Push', stackLL.getTop().toString());
+console.log('#StackLL: Push', stackLL.peek().toString());
 console.log('#StackLL: Pop', stackLL.pop().toString());
-console.log('#StackLL: Print', stackLL.getTop().toString());
+console.log('#StackLL: Print', stackLL.peek().toString());
 // Push item onto the StackLL
 console.log('#StackLL: Pop', stackLL.pop().toString());
 stackLL.push(4);
-console.log('#StackLL: Push', stackLL.getTop().toString());
+console.log('#StackLL: Push', stackLL.peek().toString());
 // Check if StackLL isEmpty
 console.log('#StackLL is EMpty:', stackLL.isEmpty());
 // Pop to empty StackLL
