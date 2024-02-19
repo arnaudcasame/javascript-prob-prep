1a6b42b0d74b8880b8095ed10aac5f7848f68619
Tizekod <noon.tizekod@gmail.com>
2024-02-18 23:22:31 -0500

    Solves leetcode problem #1043

--- leetcode/partition-array-maximum-sum-1043/partition-array.spec.js
+++ leetcode/partition-array-maximum-sum-1043/partition-array.spec.js
@@ -3,15 +3,15 @@ import {maxSumAfterPartitioning} from './partition-array.js';
 import {expect} from 'chai';
 
 export default describe('#1043: Partition Array for Maximum Sum', function() {
-    xit('Should return 84 for array: [1,15,7,9,2,5,10]', function() {
+    it('Should return 84 for array: [1,15,7,9,2,5,10]', function() {
         expect(maxSumAfterPartitioning([1, 15, 7, 9, 2, 5, 10], 3)).to.equal(84);
     });
 
-    xit('Should return 83 for array: [1,4,1,5,7,3,6,1,9,9,3]', function() {
+    it('Should return 83 for array: [1,4,1,5,7,3,6,1,9,9,3]', function() {
         expect(maxSumAfterPartitioning([1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4)).to.equal(83);
     });
 
-    xit('Should return 1 for array: [1]', function() {
+    it('Should return 1 for array: [1]', function() {
         expect(maxSumAfterPartitioning([1], 1)).to.equal(1);
     });
 });
