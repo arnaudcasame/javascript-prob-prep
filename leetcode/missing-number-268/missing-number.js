8787981055f782beec6b9ec6581774164b827272
Tizekod <noon.tizekod@gmail.com>
2024-02-19 21:45:18 -0500

    Solves leetcode problem #268

--- leetcode/missing-number-268/missing-number.js
+++ leetcode/missing-number-268/missing-number.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 export const missingNumber = function(nums) {
-    return 0;
+    for (let i = 0; i < nums.length + 1; i++) {
+        if (!nums.includes(i)) {
+            return i;
+        }
+    }
+    return nums[nums.length-1] + 1;
+};
+
+export const missingNumberO = function(nums) {
+    const bucket = {};
+    for (const num of nums) {
+        bucket[num] = 1;
+    }
+    for (let i = 0; i < nums.length + 1; i++) {
+        if (!bucket[i]) {
+            return i;
+        }
+    }
+    return nums[nums.length-1] + 1;
 };
