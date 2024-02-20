45a9161e9f65db00b568d71862efbc56af9a49f8
Tizekod <noon.tizekod@gmail.com>
2024-02-19 23:36:32 -0500

    Implements tree node's helper methods

--- data-structures/tree-node.js
+++ data-structures/tree-node.js
@@ -6,14 +6,15 @@
 export class TreeNode {
     /**
      * The TreeNode initializer
+     * @param {string} name the node's name
      */
-    constructor() {
+    constructor(name) {
         /**
          * The name of the TreeNode
          * @type {string}
          * @private
          */
-        this.name_ = null;
+        this.name_ = name;
         /**
          * The list of tree nodes that are
          * children to the current TreeNode
@@ -21,5 +22,29 @@ export class TreeNode {
          * @private
          */
         this.children_ = [];
+    }
+
+    /**
+     * Inserts node to the children's list
+     * @param {TreeNode} node the node to insert
+     */
+    insertNode(node) {
+        this.children_.push(node);
+    }
+
+    /**
+     * Returns the adjacency list
+     * @return {TreeNode[]}
+     */
+    getChildren() {
+        return this.children_;
+    }
+
+    /**
+     * Prints the current node's name
+     * in the console
+     */
+    print() {
+        console.log(this.name_);
     }
 }
