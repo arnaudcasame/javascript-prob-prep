82bed62a3d229c51680a999b8459cfcef148fd55
Tizekod <noon.tizekod@gmail.com>
2024-02-20 20:59:57 -0500

    Adds leetcode problem #201

---
+++ leetcode/bitwise-AND-numbers-range-201/bitwise-and-numbers.spec.js
@@ -0,0 +1,17 @@
+import {rangeBitwiseAnd} from './bitwise-and-numbers.js';
+import {expect} from 'chai';
+
+
+export default describe('#201: Bitwise AND of numbers range', function() {
+    xit('Should return 4 for left = 5 and right = 7', function() {
+        expect(rangeBitwiseAnd(5, 7)).to.equal(4);
+    });
+
+    xit('Should return 0 for left = 0 and right = 0', function() {
+        expect(rangeBitwiseAnd(0, 0)).to.equal(0);
+    });
+
+    xit('Should return 0 for left = 1 and right = 2147483647', function() {
+        expect(rangeBitwiseAnd(1, 2147483647)).to.equal(0);
+    });
+});
