39f660a1476b1172a7e49b163ef80e3f8d386c11
Tizekod <noon.tizekod@gmail.com>
2024-02-22 23:07:51 -0500

    Solves leetcode problem #997

--- leetcode/find-the town-judge-997/find-judge.js
+++ leetcode/find-the town-judge-997/find-judge.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 export const findJudge = function(n, trust) {
-    return 0;
+    const trusters = new Array(n + 1).fill(0);
+
+    for (const [t, j] of trust) {
+        trusters[t] -= 1;
+        trusters[j] += 1;
+    }
+
+    for (let i = 1; i <= n; i++) {
+        if (trusters[i] === n - 1) {
+            return i;
+        }
+    }
+
+    return -1;
 };
--- leetcode/find-the town-judge-997/find-judge.spec.js
+++ leetcode/find-the town-judge-997/find-judge.spec.js
@@ -1,16 +1,21 @@
+/* eslint-disable max-len */
 import {findJudge} from './find-judge.js';
 import {expect} from 'chai';
 
 export default describe('#997: Find the Town Judge', function() {
-    xit('Should return 2 for: [[1, 2]]', function() {
+    it('Should return 2 for: [[1, 2]]', function() {
         expect(findJudge(2, [[1, 2]])).to.equal(2);
     });
 
-    xit('Should return 3 for: [[1,3],[2,3]]', function() {
+    it('Should return 3 for: [[1,3],[2,3]]', function() {
         expect(findJudge(3, [[1, 3], [2, 3]])).to.equal(3);
     });
 
-    xit('Should return -1 for: [[1,3],[2,3],[3,1]]', function() {
+    it('Should return -1 for: [[1,3],[2,3],[3,1]]', function() {
         expect(findJudge(3, [[1, 3], [2, 3], [3, 1]])).to.equal(-1);
+    });
+
+    it('Should return 3 for: [[1,3],[1,4],[2,3],[2,4],[4,3]]', function() {
+        expect(findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]])).to.equal(3);
     });
 });
