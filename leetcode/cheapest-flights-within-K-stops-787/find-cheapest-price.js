de3583b61a34746f5129d450fdc277378ea07242
Tizekod <noon.tizekod@gmail.com>
2024-02-23 19:24:26 -0500

    Solves leetcode problem #787

--- leetcode/cheapest-flights-within-K-stops-787/find-cheapest-price.js
+++ leetcode/cheapest-flights-within-K-stops-787/find-cheapest-price.js
@@ -7,5 +7,23 @@
  * @return {number}
  */
 export const findCheapestPrice = function(n, flights, src, dst, k) {
-    return 0;
+    let prices = new Array(n).fill(Infinity);
+    prices[src] = 0;
+
+    for (let i = 0; i < k+1; i++) {
+        const tmpPrices = prices.slice(0);
+
+        // console.log(tmpPrices);
+
+        for (const [s, d, p] of flights) {
+            if (prices[s] === Infinity) continue;
+
+            if (prices[s]+p < tmpPrices[d]) {
+                tmpPrices[d] = prices[s] + p;
+            }
+        }
+
+        prices = tmpPrices;
+    }
+    return prices[dst] === Infinity ? -1 : prices[dst];
 };
--- leetcode/cheapest-flights-within-K-stops-787/find-cheapest-price.spec.js
+++ leetcode/cheapest-flights-within-K-stops-787/find-cheapest-price.spec.js
@@ -4,15 +4,15 @@ import {findCheapestPrice} from './find-cheapest-price.js';
 
 
 export default describe('#787: Cheapest Flights Within K Stops', function() {
-    xit('Should return 700 for the cheapest price ', function() {
+    it('Should return 700 for the cheapest price ', function() {
         expect(findCheapestPrice(4, [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]], 0, 3, 1)).to.equal(700);
     });
 
-    xit('Should return 200 for the cheapest price ', function() {
+    it('Should return 200 for the cheapest price ', function() {
         expect(findCheapestPrice(3, [[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 1)).to.equal(200);
     });
 
-    xit('Should return 500 for the cheapest price', function() {
+    it('Should return 500 for the cheapest price', function() {
         expect(findCheapestPrice(3, [[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 0)).to.equal(500);
     });
 });
