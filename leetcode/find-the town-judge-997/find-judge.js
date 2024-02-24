a83c91c1df55cb5a147c43baa566091a7ac6e007
Tizekod <noon.tizekod@gmail.com>
2024-02-23 19:07:48 -0500

    Adds antoehr solution to the Leetcode problem #997

--- leetcode/find-the town-judge-997/find-judge.js
+++ leetcode/find-the town-judge-997/find-judge.js
@@ -19,3 +19,51 @@ export const findJudge = function(n, trust) {
 
     return -1;
 };
+
+export const findJudgeMySolution = function(n, trust) {
+    const src = {};
+    const dst = {};
+
+    for (const [t, j] of trust) {
+        if (src[t]) {
+            src[t].push(j);
+        } else {
+            src[t] = [j];
+        }
+        if (dst[t]) {
+            dst[t].push(j);
+        } else {
+            dst[t] = [j];
+        }
+        if (dst[j]) {
+            dst[j].push(t);
+        } else {
+            dst[j] = [t];
+        }
+        if (src[j]) {
+            src[j].push(t);
+        } else {
+            src[j] = [t];
+        }
+    }
+
+    // for (const [t, j] of trust) {
+    //     src[t].push(j);
+    //     dst[j].push(t);
+    // }
+
+    console.log(src, dst);
+
+    for (const key in src) {
+        if (src[key].length === 0 && dst[key].length === n-1) {
+            return parseInt(key);
+        }
+    }
+
+    if (trust.length === 0 && n === 1) {
+        return n;
+    }
+
+
+    return -1;
+};
--- leetcode/find-the town-judge-997/find-judge.spec.js
+++ leetcode/find-the town-judge-997/find-judge.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import {findJudge} from './find-judge.js';
+import {findJudge, findJudgeMySolution} from './find-judge.js';
 import {expect} from 'chai';
 
 export default describe('#997: Find the Town Judge', function() {
@@ -17,5 +17,29 @@ export default describe('#997: Find the Town Judge', function() {
 
     it('Should return 3 for: [[1,3],[1,4],[2,3],[2,4],[4,3]]', function() {
         expect(findJudge(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]])).to.equal(3);
+    });
+
+    it('Should return 2 for: [[1, 2]]', function() {
+        expect(findJudgeMySolution(2, [[1, 2]])).to.equal(2);
+    });
+
+    it('Should return 3 for: [[1,3],[2,3]]', function() {
+        expect(findJudgeMySolution(3, [[1, 3], [2, 3]])).to.equal(3);
+    });
+
+    it('Should return -1 for: [[1,3],[2,3],[3,1]]', function() {
+        expect(findJudgeMySolution(3, [[1, 3], [2, 3], [3, 1]])).to.equal(-1);
+    });
+
+    it('Should return 3 for: [[1,3],[1,4],[2,3],[2,4],[4,3]]', function() {
+        expect(findJudgeMySolution(4, [[1, 3], [1, 4], [2, 3], [2, 4], [4, 3]])).to.equal(3);
+    });
+
+    it('Should return 1 for: []', function() {
+        expect(findJudgeMySolution(1, [])).to.equal(1);
+    });
+
+    it('Should return -1 for: []', function() {
+        expect(findJudgeMySolution(2, [])).to.equal(-1);
     });
 });
