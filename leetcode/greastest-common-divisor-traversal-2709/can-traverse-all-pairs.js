9bca5f8a209d6708e51cf5ba424bf3fc4ce47c3b
Tizekod <noon.tizekod@gmail.com>
2024-02-25 03:28:44 -0500

    Solves leetcode problem #2709

--- leetcode/greastest-common-divisor-traversal-2709/can-traverse-all-pairs.js
+++ leetcode/greastest-common-divisor-traversal-2709/can-traverse-all-pairs.js
@@ -1,7 +1,63 @@
 /**
+ * HARD
+ */
+/**
  * @param {number[]} nums
  * @return {boolean}
  */
 export const canTraverseAllPairs = function(nums) {
+    let [i, j] = [0, 1];
+    const graph = {};
+
+    if (nums.length < 2) return true;
+
+    while (i < nums.length -1) {
+        const gcdResult = gcd(nums[i], nums[j]);
+        // console.log(nums[i], nums[j], gcdResult);
+        if (gcdResult > 1) {
+            if (!(i in graph)) graph[i] = [];
+            if (!(j in graph)) graph[j] = [];
+
+            graph[j].push(i);
+            graph[i].push(j);
+        } else {
+            if (!(i in graph)) graph[i] = [];
+            if (!(j in graph)) graph[j] = [];
+        }
+        if (j === nums.length -1) {
+            i++;
+            j = i+1;
+        } else {
+            j++;
+        }
+    }
+    let count = 0;
+    const visited = new Set();
+    // console.log(graph);
+    for (const node in graph) {
+        if (dfs(graph, parseInt(node), visited)) {
+            count += 1;
+        }
+    }
+    return count === 1;
+};
+
+const dfs = (graph, src, visited) => {
+    if (visited.has(src)) return false;
+
+    visited.add(src);
+
+    for (const neighbor of graph[src]) {
+        dfs(graph, neighbor, visited);
+    }
+
+    return true;
+};
+
+
+const gcd = (a, b) => (b === 0) ? a : gcd(b, a % b);
+
+
+export const canTraverseAllPairsO = function(nums) {
     return false;
 };
--- leetcode/greastest-common-divisor-traversal-2709/can-traverse-all-pairs.spec.js
+++ leetcode/greastest-common-divisor-traversal-2709/can-traverse-all-pairs.spec.js
@@ -2,15 +2,31 @@ import {expect} from 'chai';
 import {canTraverseAllPairs} from './can-traverse-all-pairs.js';
 
 export default describe('#2709: Greatest Common Divisor Traversal', function() {
-    xit('Should return true for [2, 3, 6]', function() {
+    it('Should return true for [2, 3, 6]', function() {
         expect(canTraverseAllPairs([2, 3, 6])).to.be.true;
     });
 
-    xit('Should return false for [3,9,5]', function() {
+    it('Should return false for [3,9,5]', function() {
         expect(canTraverseAllPairs([3, 9, 5])).to.be.false;
     });
 
-    xit('Should return true for [4,3,12,8]', function() {
+    it('Should return true for [4,3,12,8]', function() {
         expect(canTraverseAllPairs([4, 3, 12, 8])).to.be.true;
+    });
+
+    it('Should return false for [28, 39]', function() {
+        expect(canTraverseAllPairs([28, 39])).to.be.false;
+    });
+
+    it('Should return false for [21,88,75]', function() {
+        expect(canTraverseAllPairs([21, 88, 75])).to.be.false;
+    });
+
+    it('Should return false for [1, 1]', function() {
+        expect(canTraverseAllPairs([1, 1])).to.be.false;
+    });
+
+    it('Should return true for [10]', function() {
+        expect(canTraverseAllPairs([10])).to.be.true;
     });
 });
