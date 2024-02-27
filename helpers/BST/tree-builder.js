dce7dfab53a1129e9dfb4c6025aa04c87d0c1efb
Tizekod <noon.tizekod@gmail.com>
2024-02-26 23:39:25 -0500

    Makes tree builder versatile (use indices or values)

--- helpers/BST/tree-builder.js
+++ helpers/BST/tree-builder.js
@@ -1,14 +1,32 @@
-export const buildTree = (arr)=>{
+/**
+ * Builds a Binary Tree from an array using
+ * indices when mode is 'i' or values when mode is 'v'
+ * @param {T} arr
+ * @param {string} mode can be 'i' or 'v'
+ * @return {Object}
+ */
+export const buildTree = (arr, mode)=>{
     const tree = {};
     let i = 0;
 
-    while (i < arr.length) {
+    while (i < arr.length && mode === 'i') {
         if (!(i in tree)) {
             tree[i] = [];
         }
 
         arr[(i*2)+1] !== undefined && tree[i].push((i*2)+1);
         arr[(i*2)+2] !== undefined && tree[i].push((i*2)+2);
+
+        i++;
+    }
+
+    while (i < arr.length && mode === 'v') {
+        if (!(arr[i] in tree)) {
+            tree[arr[i]] = [];
+        }
+
+        arr[(i*2)+1] !== undefined && tree[arr[i]].push(arr[(i*2)+1]);
+        arr[(i*2)+2] !== undefined && tree[arr[i]].push(arr[(i*2)+2]);
 
         i++;
     }
