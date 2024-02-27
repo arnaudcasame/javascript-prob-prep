1fc31db21e0cb31f3b478dd4c8f94c897d653a24
Tizekod <noon.tizekod@gmail.com>
2024-02-26 23:39:45 -0500

    Solves leetcode problem #543

--- leetcode/diameter-of-binary-tree-543/diameter-binary-tree.js
+++ leetcode/diameter-of-binary-tree-543/diameter-binary-tree.js
@@ -1,3 +1,5 @@
+import {buildTree} from '../../helpers/BST/tree-builder.js';
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -11,5 +13,23 @@
  * @return {number}
  */
 export const diameterOfBinaryTree = function(root) {
-    return 0;
+    const tree = buildTree(root, 'v');
+
+    const result = [0];
+
+    dfs(tree, root[0], result);
+
+    // console.log(result);
+    return result[0];
+};
+
+const dfs = (graph, root, result) => {
+    if (!root) return -1;
+
+    const left = dfs(graph, graph[root][0], result);
+    const right = dfs(graph, graph[root][1], result);
+
+    result[0] = Math.max(result[0], left + right + 2);
+
+    return 1 + Math.max(left, right);
 };
--- leetcode/diameter-of-binary-tree-543/diameter-binary-tree.spec.js
+++ leetcode/diameter-of-binary-tree-543/diameter-binary-tree.spec.js
@@ -2,11 +2,11 @@ import {expect} from 'chai';
 import {diameterOfBinaryTree} from './diameter-binary-tree.js';
 
 export default describe('#543: Diameter of Binary Tree', function() {
-    xit('Should return 3 for tree: [1,2,3,4,5]', function() {
+    it('Should return 3 for tree: [1,2,3,4,5]', function() {
         expect(diameterOfBinaryTree([1, 2, 3, 4, 5])).to.equal(3);
     });
 
-    xit('Should return 1 for tree: [1,2]', function() {
+    it('Should return 1 for tree: [1,2]', function() {
         expect(diameterOfBinaryTree([1, 2])).to.equal(1);
     });
 });
