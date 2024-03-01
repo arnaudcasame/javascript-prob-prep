8b1c2d71f4f1c662a367cc3454a5cf6da96abedb
Tizekod <noon.tizekod@gmail.com>
2024-02-29 23:24:52 -0500

    Solves leetcode problem #2864

--- leetcode/maximum-odd-binary-number-2864/maximum-odd-binary-number.js
+++ leetcode/maximum-odd-binary-number-2864/maximum-odd-binary-number.js
@@ -3,5 +3,18 @@
  * @return {string}
  */
 export const maximumOddBinaryNumber = function(s) {
-    return '';
+    const arr = s.split('');
+    let left = 0;
+    let right = 0;
+    while (right <= arr.length-1) {
+        if (arr[right] === '1') {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+        }
+
+        right++;
+    }
+    [arr[left-1], arr[arr.length-1]] = [arr[arr.length-1], arr[left-1]];
+    // console.log(s, left, arr.join(''));
+    return arr.join('');
 };
--- leetcode/maximum-odd-binary-number-2864/maximum-odd-binary-number.spec.js
+++ leetcode/maximum-odd-binary-number-2864/maximum-odd-binary-number.spec.js
@@ -2,11 +2,19 @@ import {maximumOddBinaryNumber} from './maximum-odd-binary-number.js';
 import {expect} from 'chai';
 
 export default describe('#2864: Maximum Odd Binary Number', function() {
-    xit('Should return String 001 for input: 010', function() {
+    it('Should return String 001 for input: 010', function() {
         expect(maximumOddBinaryNumber('010')).to.equal('001');
     });
 
-    xit('Should return String 1001 for input: 0101', function() {
+    it('Should return String 1001 for input: 0101', function() {
         expect(maximumOddBinaryNumber('0101')).to.equal('1001');
+    });
+
+    it('Should return String 11100000001 for input: 10000101001', function() {
+        expect(maximumOddBinaryNumber('10000101001')).to.equal('11100000001');
+    });
+
+    it('Should return String 1 for input: 1', function() {
+        expect(maximumOddBinaryNumber('1')).to.equal('1');
     });
 });
