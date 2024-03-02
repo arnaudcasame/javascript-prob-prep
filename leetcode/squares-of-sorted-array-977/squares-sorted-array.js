03ecd94428081db42a03fc6d1ce48b52abc406ea
Tizekod <noon.tizekod@gmail.com>
2024-03-02 00:52:46 -0500

    Solves leetcode problem #977

--- leetcode/squares-of-sorted-array-977/squares-sorted-array.js
+++ leetcode/squares-of-sorted-array-977/squares-sorted-array.js
@@ -3,5 +3,11 @@
  * @return {number[]}
  */
 export const sortedSquares = function(nums) {
-    return [];
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] * nums[i];
+    }
+
+    nums.sort((a, b) => a-b);
+
+    return nums;
 };
--- leetcode/squares-of-sorted-array-977/squares-sorted-array.spec.js
+++ leetcode/squares-of-sorted-array-977/squares-sorted-array.spec.js
@@ -3,11 +3,11 @@ import {sortedSquares} from './squares-sorted-array.js';
 import {expect} from 'chai';
 
 export default describe('#977: Squares of a Sorted Array', function() {
-    xit('Should return [0, 1, 9, 16, 100]', function() {
+    it('Should return [0, 1, 9, 16, 100]', function() {
         expect(sortedSquares([-4, -1, 0, 3, 10])).to.deep.equal([0, 1, 9, 16, 100]);
     });
 
-    xit('Should return [4,9,9,49,121]', function() {
+    it('Should return [4,9,9,49,121]', function() {
         expect(sortedSquares([-7, -3, 2, 3, 11])).to.deep.equal([4, 9, 9, 49, 121]);
     });
 });
