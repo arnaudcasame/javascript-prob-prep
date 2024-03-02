2f1ff1ba37c837caecd14906ec7c6fd94106cb77
Tizekod <noon.tizekod@gmail.com>
2024-03-02 17:37:24 -0500

    Adds a clearer description for problem

--- cci/trees-graphs/4.2-minimal-tree/minimal-tree.spec.js
+++ cci/trees-graphs/4.2-minimal-tree/minimal-tree.spec.js
@@ -5,7 +5,7 @@ import {minimalTree} from './minimal-tree.js';
 import {expect} from 'chai';
 
 export default describe('#4.2: Minimal Tree', function() {
-    xit('Should return ', function() {
+    xit('Should return the root as first element', function() {
         // Pre-Order traversal of the tree should result in the expected array
         expect(minimalTree([1, 2, 3, 4, 5, 6, 7, 8, 9])).to.deep.equal([5, 1, 2, 3, 4, 6, 7, 8, 9]);
     });
