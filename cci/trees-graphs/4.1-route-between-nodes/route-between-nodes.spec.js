bb762f5b233fb06d5b56ada91e893c3ec619ac0c
Tizekod <noon.tizekod@gmail.com>
2024-03-02 16:21:53 -0500

    Adds more test cases

--- cci/trees-graphs/4.1-route-between-nodes/route-between-nodes.spec.js
+++ cci/trees-graphs/4.1-route-between-nodes/route-between-nodes.spec.js
@@ -3,10 +3,60 @@ import {expect} from 'chai';
 
 export default describe('#4.1: Route Between Nodes', function() {
     xit('Should return true for: ', function() {
-        expect(routeBetweenNodes(null, null)).to.be.true;
+        const graph = {
+            f: ['g', 'i'],
+            g: ['h'],
+            h: [],
+            i: ['g', 'k'],
+            j: ['i'],
+            k: [],
+        };
+        expect(routeBetweenNodes(graph, 'f', 'k')).to.be.true;
     });
 
-    xit('Should return true for: ', function() {
-        expect(routeBetweenNodes(null, null)).to.be.true;
+    xit('Should return false for: f <--> j', function() {
+        const graph = {
+            f: ['g', 'i'],
+            g: ['h'],
+            h: [],
+            i: ['g', 'k'],
+            j: ['i'],
+            k: [],
+        };
+        expect(routeBetweenNodes(graph, 'f', 'j')).to.be.false;
+    });
+
+    xit('Should return true for: i <---> h', function() {
+        const graph = {
+            f: ['g', 'i'],
+            g: ['h'],
+            h: [],
+            i: ['g', 'k'],
+            j: ['i'],
+            k: [],
+        };
+        expect(routeBetweenNodes(graph, 'i', 'h')).to.be.true;
+    });
+
+    xit('Should return true for: v <---> h', function() {
+        const graph = {
+            v: ['x', 'w'],
+            w: [],
+            x: [],
+            y: ['z'],
+            z: [],
+        };
+        expect(routeBetweenNodes(graph, 'v', 'h')).to.be.true;
+    });
+
+    xit('Should return false for: v <---> z', function() {
+        const graph = {
+            v: ['x', 'w'],
+            w: [],
+            x: [],
+            y: ['z'],
+            z: [],
+        };
+        expect(routeBetweenNodes(graph, 'v', 'z')).to.be.false;
     });
 });
