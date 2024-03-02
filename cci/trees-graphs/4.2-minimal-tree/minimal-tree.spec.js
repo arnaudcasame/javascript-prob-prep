d2545e9324ad94413fd80eb9b893bff0dd64a84f
Tizekod <noon.tizekod@gmail.com>
2024-03-02 15:52:04 -0500

    Updates minimal tree problem

--- cci/trees-graphs/4.2-minimal-tree/minimal-tree.spec.js
+++ cci/trees-graphs/4.2-minimal-tree/minimal-tree.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 import {BinarySearchTree} from '../../../data-structures/binary-search-tree.js';
 import {minimalTree} from './minimal-tree.js';
@@ -5,6 +6,7 @@ import {expect} from 'chai';
 
 export default describe('#4.2: Minimal Tree', function() {
     xit('Should return ', function() {
-        expect(minimalTree([1, 2, 3, 4, 5, 6, 7, 8, 9])).to.equal(new BinarySearchTree(0));
+        // Pre-Order traversal of the tree should result in the expected array
+        expect(minimalTree([1, 2, 3, 4, 5, 6, 7, 8, 9])).to.deep.equal([5, 1, 2, 3, 4, 6, 7, 8, 9]);
     });
 });
