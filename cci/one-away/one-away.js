0244f79ea2620442f576fa750e4c19fd333045f3
Tizekod <noon.tizekod@gmail.com>
2024-03-02 02:55:53 -0500

    Fixes the 1.5: One away problem

--- cci/one-away/one-away.js
+++ cci/one-away/one-away.js
@@ -1,1 +1,10 @@
-export const oneAway = function(s1, s2){};
+/**
+ * Check if they are one edit
+ * (or zero edits) away from each other.
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+export const oneAway = function(s1, s2) {
+    return false;
+};
--- cci/one-away/one-away.spec.js
+++ cci/one-away/one-away.spec.js
@@ -1,19 +1,20 @@
-import { oneAway } from "./one-away.js";
+import {oneAway} from './one-away.js';
+import {expect} from 'chai';
 
-export default describe('One Away', function(){
-    xit('Should return true for (pale, ple)', function(){
-        expect(oneAway('pale', 'ple')).toEqual(true);
+export default describe('#1.5: One Away', function() {
+    xit('Should return true for (pale, ple)', function() {
+        expect(oneAway('pale', 'ple')).to.be.true;
     });
 
-    xit('Should return true for (pales, pale)', function(){
-        expect(oneAway('pales', 'pale')).toEqual(true);
+    xit('Should return true for (pales, pale)', function() {
+        expect(oneAway('pales', 'pale')).to.be.true;
     });
 
-    xit('Should return true for (pale, bale)', function(){
-        expect(oneAway('pale', 'bale')).toEqual(true);
+    xit('Should return true for (pale, bale)', function() {
+        expect(oneAway('pale', 'bale')).to.be.true;
     });
 
-    xit('Should return false for (pale, bake)', function(){
-        expect(oneAway('pale', 'bake')).toEqual(false);
+    xit('Should return false for (pale, bake)', function() {
+        expect(oneAway('pale', 'bake')).to.be.false;
     });
 });
