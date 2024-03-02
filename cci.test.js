6a00d016fb36c9b9a7a39a421a839138d89b87eb
Tizekod <noon.tizekod@gmail.com>
2024-03-02 15:03:01 -0500

    Adds chapter number to arrays & strings problem

--- cci.test.js
+++ cci.test.js
@@ -2,11 +2,11 @@
 import './cci/strings-arrays/0.0-common-set-members/common-set-members.spec.js';
 import './cci/strings-arrays/1.1-is-unique/is-unique.spec.js';
 import './cci/strings-arrays/1.2-check-permutation/check-permutation.spec.js';
-import './cci/strings-arrays/urlify/urlify.spec.js';
-import './cci/strings-arrays/palindrome-permutation/palindrome-permutation.spec.js';
-import './cci/strings-arrays/one-away/one-away.spec.js';
-import './cci/strings-arrays/string-compression/string-compressiion.spec.js';
-import './cci/strings-arrays/string-rotation/string-rotation.spec.js';
+import './cci/strings-arrays/1.3-urlify/urlify.spec.js';
+import './cci/strings-arrays/1.4-palindrome-permutation/palindrome-permutation.spec.js';
+import './cci/strings-arrays/1.5-one-away/one-away.spec.js';
+import './cci/strings-arrays/1.6-string-compression/string-compressiion.spec.js';
+import './cci/strings-arrays/1.9-string-rotation/string-rotation.spec.js';
 import './cci/linked-list/2.1-remove-duplicates/remove-dups.spec.js';
 import './cci/linked-list/2.2-kth-to-last/kth-to-last.spec.js';
 import './cci/linked-list/2.3-delete-middle-node/delete-middle-node.spec.js';
