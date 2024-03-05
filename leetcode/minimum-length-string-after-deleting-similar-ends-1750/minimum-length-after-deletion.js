7b333fda3a505493a05ef93095225ce5248356c9
Tizekod <noon.tizekod@gmail.com>
2024-03-04 21:43:11 -0500

    Solves leetcode problem #1750

--- leetcode/minimum-length-string-after-deleting-similar-ends-1750/minimum-length-after-deletion.js
+++ leetcode/minimum-length-string-after-deleting-similar-ends-1750/minimum-length-after-deletion.js
@@ -3,5 +3,35 @@
  * @return {number}
  */
 export const minimumLength = function(s) {
-    return 0;
+    let [left, right] = [0, s.length-1];
+    while (left < right) {
+        if (s[left] === s[right]) {
+            if (s.length === 2) {
+                s = '';
+                left = 1;
+                right = 0;
+                continue;
+            }
+
+            const letter = s[left];
+
+            while (s[left] === letter && right >= left) {
+                left++;
+            }
+
+            while (s[right] === letter && right >= left) {
+                right--;
+            }
+
+            // console.log(s.substring(left, right+1));
+
+            s = s.substring(left, right+1);
+            left = 0;
+            right = s.length-1;
+        } else {
+            break;
+        }
+    }
+    // console.log(s, left, right);
+    return s.length;
 };
--- leetcode/minimum-length-string-after-deleting-similar-ends-1750/minimum-length-after-deletion.spec.js
+++ leetcode/minimum-length-string-after-deleting-similar-ends-1750/minimum-length-after-deletion.spec.js
@@ -3,15 +3,23 @@ import {minimumLength} from './minimum-length-after-deletion.js';
 import {expect} from 'chai';
 
 export default describe('#1750: Minimum Length of String after Deleting Similar Ends', function() {
-    xit('Should return 2 for (ca)', function() {
+    it('Should return 2 for (ca)', function() {
         expect(minimumLength('ca')).to.equal(2);
     });
 
-    xit('Should return 0 for (cabaabac)', function() {
+    it('Should return 0 for (cabaabac)', function() {
         expect(minimumLength('cabaabac')).to.equal(0);
     });
 
-    xit('Should return 3 for (aabccabba)', function() {
+    it('Should return 3 for (aabccabba)', function() {
         expect(minimumLength('aabccabba')).to.equal(3);
+    });
+
+    it('Should return 0 for (abbbbbbbbbbbbbbbbbbba)', function() {
+        expect(minimumLength('abbbbbbbbbbbbbbbbbbba')).to.equal(0);
+    });
+
+    it('Should return 1 for (bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb)', function() {
+        expect(minimumLength('bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb')).to.equal(1);
     });
 });
