cc826f606ae93e51a5d3e6b52f9e681d7d15598f
Tizekod <noon.tizekod@gmail.com>
2024-03-06 00:59:16 -0500

    Solves Leetcode problem #141

--- leetcode/linked-list-cycle-141/linked-list-cycle.js
+++ leetcode/linked-list-cycle-141/linked-list-cycle.js
@@ -3,5 +3,16 @@
  * @return {boolean}
  */
 export const hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (fast === slow) {
+            return true;
+        }
+    }
     return false;
 };
--- leetcode/linked-list-cycle-141/linked-list-cycle.spec.js
+++ leetcode/linked-list-cycle-141/linked-list-cycle.spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai';
 import {ListNode} from '../../data-structures/list-node.js';
 
 export default describe('#141: Linked List Cycle', function() {
-    xit('Should return true', function() {
+    it('Should return true if Linked List has cycle', function() {
         const tail = new ListNode(8);
         const cycle = new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, tail))))));
         tail.next = cycle;
@@ -12,13 +12,13 @@ export default describe('#141: Linked List Cycle', function() {
         expect(hasCycle(head)).to.be.true;
     });
 
-    xit('Should return false', function() {
+    it('Should return false if Linked List has no cycle', function() {
         const cycle = new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8)))))));
         const head = new ListNode(1, cycle);
         expect(hasCycle(head)).to.be.false;
     });
 
-    xit('Should return true', function() {
+    it('Should return true if Linked List has cycle', function() {
         const tail = new ListNode(40);
         const cycle = new ListNode(27, new ListNode(34, new ListNode(22, new ListNode(52, new ListNode(35, new ListNode(28, tail))))));
         tail.next = cycle;
