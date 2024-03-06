891f49c711f3c5de11ebb4c1ab11a5d944d7aded
Tizekod <noon.tizekod@gmail.com>
2024-03-06 18:36:41 -0500

    Adds more use cases to the test suite

--- cci/trees-graphs/4.2-minimal-tree/minimal-tree.spec.js
+++ cci/trees-graphs/4.2-minimal-tree/minimal-tree.spec.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
-import {BinarySearchTree} from '../../../data-structures/binary-search-tree.js';
 import {minimalTree} from './minimal-tree.js';
 import {expect} from 'chai';
 
 export default describe('#4.2: Minimal Tree', function() {
     xit('Should return the root as first element', function() {
         // Pre-Order traversal of the tree should result in the expected array
-        expect(minimalTree([1, 2, 3, 4, 5, 6, 7, 8, 9])).to.deep.equal([5, 1, 2, 3, 4, 6, 7, 8, 9]);
+        expect(minimalTree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])).to.deep.equal([6, 3, 9, 1, 4, 7, 10, 2, 5, 8, 11]);
+    });
+
+    xit('Should return the root as first element', function() {
+        // Pre-Order traversal of the tree should result in the expected array
+        const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+        expect(minimalTree(sortedArray)).to.deep.equal([15, 7, 23, 3, 11, 19, 27, 1, 5, 9, 13, 17, 21, 25, 29]);
+    });
+
+    xit('Should return the root as first element', function() {
+        // Pre-Order traversal of the tree should result in the expected array
+        const sortedArray = [2, 4, 6, 8, 10, 12];
+        expect(minimalTree(sortedArray)).to.deep.equal([6, 2, 10, 4, 8, 12]);
     });
 });
