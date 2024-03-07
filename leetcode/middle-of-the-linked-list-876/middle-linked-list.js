12bbe2300bacd455fb6ae9f1a6f3b9ad666b6b03
Tizekod <noon.tizekod@gmail.com>
2024-03-07 00:39:15 -0500

    Adds Leetcode problem #876

---
+++ leetcode/middle-of-the-linked-list-876/middle-linked-list.js
@@ -0,0 +1,15 @@
+import {ListNode} from '../../data-structures/list-node.js';
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+export const middleNode = function(head) {
+    return new ListNode(0);
+};
