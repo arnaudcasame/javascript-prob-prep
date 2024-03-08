0f66157a2c1b4825c36196b15a6b331b0660a89a
Tizekod <noon.tizekod@gmail.com>
2024-03-07 23:47:49 -0500

    Solves Leetcode problem #3005

--- leetcode/count-elements-w-max-frequency-3005/count-elements-max-freq.js
+++ leetcode/count-elements-w-max-frequency-3005/count-elements-max-freq.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 export const maxFrequencyElements = function(nums) {
-    return 0;
+    const bucket = {};
+    for (const num of nums) {
+        if (!(num in bucket)) {
+            bucket[num] = 0;
+        }
+        bucket[num] += 1;
+    }
+
+    let lastFrequence = 0; let count = 0;
+    for (const key in bucket) {
+        if (bucket[key] > lastFrequence) {
+            lastFrequence = bucket[key];
+            count = 0;
+            count += lastFrequence;
+        } else if (bucket[key] === lastFrequence) {
+            count += lastFrequence;
+        }
+    }
+    console.log(bucket);
+    return count;
 };
--- leetcode/count-elements-w-max-frequency-3005/count-elements-max-freq.spec.js
+++ leetcode/count-elements-w-max-frequency-3005/count-elements-max-freq.spec.js
@@ -3,11 +3,11 @@ import {expect} from 'chai';
 import {maxFrequencyElements} from './count-elements-max-freq.js';
 
 export default describe('#3005: Count Elements With Maximum Frequency', function() {
-    xit('Should return 4 for: [1,2,2,3,1,4]', function() {
+    it('Should return 4 for: [1,2,2,3,1,4]', function() {
         expect(maxFrequencyElements([1, 2, 2, 3, 1, 4])).to.equal(4);
     });
 
-    xit('Should return 5 for: [1,2,3,4,5]', function() {
+    it('Should return 5 for: [1,2,3,4,5]', function() {
         expect(maxFrequencyElements([1, 2, 3, 4, 5])).to.equal(5);
     });
 });
