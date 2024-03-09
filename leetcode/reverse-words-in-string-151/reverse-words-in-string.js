ec5a2e33a8f36da236fdd786b21bc8078f0e76dc
Tizekod <noon.tizekod@gmail.com>
2024-03-09 18:18:16 -0500

    Solves the Leetcode problem #151

--- leetcode/reverse-words-in-string-151/reverse-words-in-string.js
+++ leetcode/reverse-words-in-string-151/reverse-words-in-string.js
@@ -1,7 +1,26 @@
+/* eslint-disable max-len */
+/* eslint-disable for-direction */
 /**
  * @param {string} s
  * @return {string}
  */
 export const reverseWords = function(s) {
-    return '';
+    let sentence = '';
+    let spacing = true;
+    let word = '';
+    for (let i = s.length-1; i >= 0; i--) {
+        const char = s[i];
+
+        if (char !== ' ') {
+            word = char + word;
+            spacing = false;
+        } else if (!spacing && word !== '') {
+            sentence = sentence === '' ? word : sentence + char + word;
+            word = '';
+            spacing = true;
+        }
+    }
+    sentence = word !== '' ? (sentence === '' ? word : sentence + ' ' + word) : sentence;
+    // console.log('/' + sentence + '/', '*' + word + '*');
+    return sentence;
 };
--- leetcode/reverse-words-in-string-151/reverse-words-in-string.spec.js
+++ leetcode/reverse-words-in-string-151/reverse-words-in-string.spec.js
@@ -3,15 +3,19 @@ import {reverseWords} from './reverse-words-in-string.js';
 
 
 export default describe('#151: Reverse Words in a String', function() {
-    xit('Should return `blue is sky the`', function() {
+    it('Should return `blue is sky the`', function() {
         expect(reverseWords('the sky is blue')).to.equal('blue is sky the');
     });
 
-    xit('Should return `world hello`', function() {
+    it('Should return `world hello`', function() {
         expect(reverseWords('  hello world  ')).to.equal('world hello');
     });
 
-    xit('Should return `example good a`', function() {
+    it('Should return `example good a`', function() {
         expect(reverseWords('a good   example')).to.equal('example good a');
+    });
+
+    it('Should return `EPY2giL`', function() {
+        expect(reverseWords('EPY2giL')).to.equal('EPY2giL');
     });
 });
