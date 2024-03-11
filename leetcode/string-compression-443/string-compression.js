3e0532b704e27bcb88e2ff952eddc4f82b3bfbac
Tizekod <noon.tizekod@gmail.com>
2024-03-11 03:25:09 -0400

    Solves the Leetcode problem #443

--- leetcode/string-compression-443/string-compression.js
+++ leetcode/string-compression-443/string-compression.js
@@ -3,6 +3,40 @@
  * @return {number}
  */
 const compress = function(chars) {
+    let s = '';
+    let currentChar = '';
+    let currentCount = 0;
+    const length = chars.length;
+    for (let i = 0; i < length; i++) {
+        const char = chars[i];
+        if (char !== currentChar) {
+            if (currentChar !== '' && currentCount > 1) {
+                s += currentChar + currentCount;
+            } else if (currentCount == 1) {
+                s += currentChar;
+            }
+
+            currentChar = char;
+            currentCount = 1;
+        } else if (char === currentChar) {
+            currentCount += 1;
+        }
+    }
+
+    if (currentChar !== '' && currentCount > 1) {
+        s += currentChar + currentCount;
+    } else if (currentCount == 1) {
+        s += currentChar;
+    }
+
+    let i=0;
+
+    while (i < s.length) {
+        chars[i] = s[i];
+        i++;
+    }
+
+    chars.splice(i);
     return 0;
 };
 
--- leetcode/string-compression-443/string-compression.spec.js
+++ leetcode/string-compression-443/string-compression.spec.js
@@ -4,11 +4,13 @@ import stringCompression from './string-compression.js';
 import sinon from 'sinon';
 
 export default describe('#443: String Compression', function() {
-    const spy = sinon.spy(stringCompression, 'compress');
+    let spy;
 
-    beforeEach(()=>{});
+    beforeEach(()=>{
+        spy = sinon.spy(stringCompression, 'compress');
+    });
 
-    xit('Should alter argument to [\'a\',\'2\',\'b\',\'2\',\'c\',\'3\']', function() {
+    it('Should alter argument to [\'a\',\'2\',\'b\',\'2\',\'c\',\'3\']', function() {
         // Argument to the Compress function
         const arg = ['a', 'a', 'b', 'b', 'c', 'c', 'c'];
 
@@ -19,7 +21,7 @@ export default describe('#443: String Compression', function() {
         expect(call.args[0]).to.deep.equal(['a', '2', 'b', '2', 'c', '3']);
     });
 
-    xit('Should alter argument to [\'a\']', function() {
+    it('Should alter argument to [\'a\']', function() {
         // Argument to the Compress function
         const arg = ['a'];
 
@@ -30,7 +32,7 @@ export default describe('#443: String Compression', function() {
         expect(call.args[0]).to.deep.equal(['a']);
     });
 
-    xit('Should alter argument to ["a", "b", "1", "2"]', function() {
+    it('Should alter argument to ["a", "b", "1", "2"]', function() {
         // Argument to the Compress function
         const arg = ['a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
 
@@ -41,5 +43,7 @@ export default describe('#443: String Compression', function() {
         expect(call.args[0]).to.deep.equal(['a', 'b', '1', '2']);
     });
 
-    afterEach(() => {});
+    afterEach(() => {
+        spy.restore();
+    });
 });
