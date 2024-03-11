b5bec1192cf7434fbbcee3599ac12bf05a59b9a8
Tizekod <noon.tizekod@gmail.com>
2024-03-11 03:19:16 -0400

    Solves the Leetcode problem #791

--- leetcode/custom-sort-string-791/custom-sort.js
+++ leetcode/custom-sort-string-791/custom-sort.js
@@ -4,5 +4,32 @@
  * @return {string}
  */
 export const customSortString = function(order, s) {
-    return '';
+    const bucket = {};
+    let result = '';
+    for (let i = 0; i < order.length; i++) {
+        const ch = order[i];
+        if (!(ch in bucket)) {
+            bucket[ch] = 0;
+        }
+    }
+
+    // console.log(bucket);
+
+    for (let i=0; i<s.length; i++) {
+        const ch = s[i];
+        if (ch in bucket) {
+            bucket[ch] += 1;
+        }
+    }
+
+    for (const ch of order) {
+        result += ch.repeat(bucket[ch]);
+    }
+
+    for (const ch of s) {
+        if (!(ch in bucket)) {
+            result += ch;
+        }
+    }
+    return result;
 };
--- leetcode/custom-sort-string-791/custom-sort.spec.js
+++ leetcode/custom-sort-string-791/custom-sort.spec.js
@@ -2,11 +2,19 @@ import {expect} from 'chai';
 import {customSortString} from './custom-sort.js';
 
 export default describe('#791: Custom Sort String', function() {
-    xit('Should return cbad', function() {
+    it('Should return cbad', function() {
         expect(customSortString('cba', 'abcd')).to.equal('cbad');
     });
 
-    xit('Should return bcad', function() {
+    it('Should return bcad', function() {
         expect(customSortString('bcafg', 'abcd')).to.equal('bcad');
+    });
+
+    it('Should return kqeep', function() {
+        expect(customSortString('kqep', 'pekeq')).to.equal('kqeep');
+    });
+
+    it('Should return kqeeeprty', function() {
+        expect(customSortString('kqep', 'pekeqrtye')).to.equal('kqeeeprty');
     });
 });
