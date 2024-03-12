3eefde44c4408de3cf9bc094cd41fe198f8496c7
Tizekod <noon.tizekod@gmail.com>
2024-03-12 03:30:53 -0400

    Solves Leetcode problem #1171

--- leetcode/remove-zero-sum-consecutive-nodes-from-ll-1171/remove-zero-sum.js
+++ leetcode/remove-zero-sum-consecutive-nodes-from-ll-1171/remove-zero-sum.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable max-len */
 import {ListNode} from '../../data-structures/list-node.js';
 /**
  * Definition for singly-linked list.
@@ -11,5 +13,29 @@ import {ListNode} from '../../data-structures/list-node.js';
  * @return {ListNode}
  */
 export const removeZeroSumSublists = function(head) {
-    return new ListNode();
+    let val = 0;
+    const tmp = new ListNode(0, head);
+    const bucket = new Map();
+    bucket.set(0, tmp);
+
+    while (head) {
+        val += head.val;
+        if (bucket.has(val)) {
+            let tmpRemove = bucket.get(val).next; let key = val;
+
+            // console.log('1', Object.keys(bucket));
+            while (tmpRemove !== head) {
+                key += tmpRemove.val;
+                // console.log('removing...', key);
+                bucket.delete(key);
+                tmpRemove = tmpRemove.next;
+            }
+            // console.log('2', Object.keys(bucket));
+            bucket.get(val).next = head.next;
+        } else {
+            bucket.set(val, head);
+        }
+        head = head.next;
+    }
+    return tmp.next;
 };
