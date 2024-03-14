3acb6f5f34e928f46df5693be2b0da747951e38d
Tizekod <noon.tizekod@gmail.com>
2024-03-14 01:07:06 -0400

    Solves Leetcode problem #392

--- leetcode/increasing-triplet-subsequence-334/increasing-triplet-subsequence.js
+++ leetcode/increasing-triplet-subsequence-334/increasing-triplet-subsequence.js
@@ -3,5 +3,17 @@
  * @return {boolean}
  */
 export const increasingTriplet = function(nums) {
+    let minimum = Infinity;
+    let medium = Infinity;
+
+    for (const num of nums) {
+        if (num <= minimum) {
+            minimum = num;
+        } else if (num <= medium) {
+            medium = num;
+        } else {
+            return true;
+        }
+    }
     return false;
 };
