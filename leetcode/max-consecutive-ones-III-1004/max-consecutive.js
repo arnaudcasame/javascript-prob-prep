289653abb4d701acc9adc3e35eec89334498e9b3
Tizekod <noon.tizekod@gmail.com>
2024-03-18 15:04:25 -0400

    Solves the Leetcode problem #1004

--- leetcode/max-consecutive-ones-III-1004/max-consecutive.js
+++ leetcode/max-consecutive-ones-III-1004/max-consecutive.js
@@ -4,5 +4,22 @@
  * @return {number}
  */
 export const longestOnes = function(nums, k) {
-    return -1;
+    let [i, j] = [0, 0];
+    let count = 0;
+
+    while (i < nums.length) {
+        if (nums[i] === 0) {
+            k--;
+        }
+        if (k < 0) {
+            if (nums[j] === 0) {
+                k++;
+            }
+            j++;
+        }
+        i++;
+        count = Math.max(count, i-j);
+    }
+
+    return count;
 };
--- leetcode/max-consecutive-ones-III-1004/max-consecutive.spec.js
+++ leetcode/max-consecutive-ones-III-1004/max-consecutive.spec.js
@@ -3,11 +3,11 @@ import {expect} from 'chai';
 import {longestOnes} from './max-consecutive.js';
 
 export default describe('#1004: Max Consecutive Ones III', function() {
-    xit('Should return 6', function() {
+    it('Should return 6', function() {
         expect(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2)).to.equal(6);
     });
 
-    xit('Should return 10', function() {
+    it('Should return 10', function() {
         expect(longestOnes([0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], 3)).to.equal(10);
     });
 });
