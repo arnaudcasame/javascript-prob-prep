bd9063d08638e9656fc8301fb8e700d17f828a4c
Tizekod <noon.tizekod@gmail.com>
2024-03-18 03:55:49 -0400

    Solves the Leetcode problem #1456

--- leetcode/maximum-number-of-vowels-in-substring-1456/maximum-number-of-vowels.js
+++ leetcode/maximum-number-of-vowels-in-substring-1456/maximum-number-of-vowels.js
@@ -4,5 +4,26 @@
  * @return {number}
  */
 export const maxVowels = function(s, k) {
-    return -1;
+    let [count, num] = [0, 0];
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    for (let i = 0; i < k; i++) {
+        if (vowels.has(s[i])) {
+            num++;
+            count = Math.max(count, num);
+        }
+    }
+
+    let [i, j] = [1, k];
+    while (j < s.length) {
+        if (vowels.has(s[j])) {
+            num++;
+        }
+        if (vowels.has(s[i-1])) {
+            num--;
+        }
+        count = Math.max(count, num);
+        j++;
+        i++;
+    }
+    return count;
 };
