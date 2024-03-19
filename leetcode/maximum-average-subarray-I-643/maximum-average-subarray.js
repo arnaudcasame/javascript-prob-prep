1f1501fe78c31a8a10c812903fc6d5e408474094
Tizekod <noon.tizekod@gmail.com>
2024-03-18 23:05:11 -0400

    Solves the Leetcode problem #643

--- leetcode/maximum-average-subarray-I-643/maximum-average-subarray.js
+++ leetcode/maximum-average-subarray-I-643/maximum-average-subarray.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 export const findMaxAverage = function(nums, k) {
-    return -1;
+    let [i, j] = [0, 0];
+    let total = 0;
+    let avg = -Infinity;
+    const n = k;
+
+    while (i < nums.length) {
+        k--;
+        total += nums[i];
+        if (k < 0) {
+            total -= nums[j];
+            k++;
+            j++;
+        }
+        i++;
+        avg = k < 1 ? Math.max(avg, total/n) : -Infinity;
+    }
+    return +avg.toFixed(5);
 };
