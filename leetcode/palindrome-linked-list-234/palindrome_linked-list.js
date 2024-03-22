7c2d6f7b59cbda1f4b7469aa9d5702913cf7f25b
Tizekod <noon.tizekod@gmail.com>
2024-03-22 02:22:17 -0400

    Adds the Leetcode problem #234

---
+++ leetcode/palindrome-linked-list-234/palindrome_linked-list.js
@@ -0,0 +1,15 @@
+import {ListNode} from '../../data-structures/list-node.js';
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+export const isPalindrome = function(head) {
+    return false;
+};
